c50a56453a235390b66bc22df25da9390dd17870
Jet_Yeh <good79324@gmail.com>
2020-09-30 10:28:56 +0800

    取得路邊租還車輛機車 增加業者icon、業者評分

--- Reposotory.Implement/StationAndCarRepository.cs
+++ Reposotory.Implement/StationAndCarRepository.cs
@@ -228,6 +228,7 @@ namespace Reposotory.Implement
             int nowCount = 0;
             string SQL = "SELECT [CarNo],[CarType],CONCAT([CarBrend],' ',[CarTypeName]) AS CarTypeName,REPLACE([PRONAME],'10載便利','') AS CarOfArea,[PRONAME] AS ProjectName,[PRICE] AS Rental,2.5 AS Mileage,0 AS Insurance,0 As InsurancePrice,0 As ShowSpecial,'' As SpecialInfo,[Latitude] ,[Longitude]";
             SQL += " ,device2TBA AS 'Power',deviceRDistance AS RemainingMileage ";
+            SQL += " ,[OperatorICon],[Score]";  // 2020.09.30 育誠 增加業者、業者評分
             SQL += " FROM  [VW_GetAllMotorAnyRentData] WITH(NOLOCK) WHERE GPSTime>=DATEADD(MINUTE,-30,GETDATE())  AND device2TBA>=30 ";
             SqlParameter[] para = new SqlParameter[2];
             string term = "";
--- iRentDBV2/dbo/Views/VW_GetAllMotorAnyRentData.sql
+++ iRentDBV2/dbo/Views/VW_GetAllMotorAnyRentData.sql
@@ -7,10 +7,12 @@ SELECT Car.[CarNo],CarStatus.[CID],[Token],[deviceType],[ACCStatus],[GPSStatus],
       ,[deviceBLE_Login],[deviceBLE_BroadCast],[devicePwr_Mode],[deviceReversing],[devicePut_Down]
       ,[devicePwr_Relay],[deviceStart_OK],[deviceHard_ACC],[deviceEMG_Break],[deviceSharp_Turn]
       ,[deviceBat_Cover],[extDeviceStatus1],[extDeviceData5],[extDeviceData6],Car.available 
-	  ,FullProj.[PROJID],[PRONAME],[PRODESC],[CarBrend],[CarTypeName],CarInfo.CarType,FullProj.[ShowStart], FullProj.[ShowEnd],FullProj.[PRSTDT],FullProj.[PRENDT],FullProj.[PRICE],FullProj.[PRICE_H]
+	  ,FullProj.[PROJID],[PRONAME],[PRODESC],[CarBrend],[CarTypeName],CarInfo.CarType,FullProj.[ShowStart], FullProj.[ShowEnd]
+	  ,FullProj.[PRSTDT],FullProj.[PRENDT],FullProj.[PRICE],FullProj.[PRICE_H]
+	  ,OperatorBase.[OperatorICon],OperatorBase.[Score]  -- 2020.09.30 育誠 增加業者、業者評分
 	 FROM TB_CarStatus AS CarStatus
-INNER JOIN 	  TB_Car AS Car WITH(NOLOCK) ON Car.CarNo=CarStatus.CarNo
+INNER JOIN TB_Car AS Car WITH(NOLOCK) ON Car.CarNo=CarStatus.CarNo
 INNER JOIN TB_CarInfo As CarInfo WITH(NOLOCK) ON CarInfo.CarNo=CarStatus.CarNo
 INNER JOIN VW_FullProjectCollection AS FullProj WITH(NOLOCK) ON Car.nowStationID=FullProj.StationID AND CarInfo.CarType=FullProj.CARTYPE AND FullProj.IOType='O' AND FullProj.PROJTYPE=4
-	  WHERE Car.available<2 AND ((FullProj.[ShowStart]<=GETDATE() AND FullProj.[ShowEnd]>GETDATE()) OR FullProj.[PRSTDT]<=GETDATE() AND FullProj.[PRENDT]>GETDATE())
-
+INNER JOIN TB_OperatorBase As OperatorBase On OperatorBase.OperatorID=Car.Operator
+WHERE Car.available<2 AND ((FullProj.[ShowStart]<=GETDATE() AND FullProj.[ShowEnd]>GETDATE()) OR FullProj.[PRSTDT]<=GETDATE() AND FullProj.[PRENDT]>GETDATE())
