9848d3aa76b2a9e58436478fe7a16a31a5522557
gogolucky168 <gogolucky168@gmail.com>
2020-09-30 16:42:30 +0800

    RefrashToken修改ApiOutPut

---
+++ WebAPI/Models/Param/Output/OAPI_RefrashToken.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Output
+{
+    public class OAPI_RefrashToken
+    {
+        /// <summary>
+        /// OAuth需使用，Token type=>Bearer
+        /// </summary>
+        public Token Token { set; get; }
+    }
+}
