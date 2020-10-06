28af8378af0f35554359862de28f7410c7eeeacd
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-10-06 11:06:10 +0800

    月租tb及輸出

---
+++ Reposotory.Implement/MonthlyRentRepository.cs
@@ -0,0 +1,88 @@
+﻿using Domain.TB;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCommon;
+
+namespace Reposotory.Implement
+{
+    /// <summary>
+    /// 月租相關
+    /// </summary>
+    public class MonthlyRentRepository : BaseRepository
+    {
+        private string _connectionString;
+        public MonthlyRentRepository(string ConnStr)
+        {
+            this.ConnectionString = ConnStr;
+        }
+        /// <summary>
+        /// 取出月租剩餘時數及優惠費率
+        /// </summary>
+        /// <param name="IDNO">身份證</param>
+        /// <param name="SD">實際取車日</param>
+        /// <param name="ED">實際還車日</param>
+        /// <param name="RateType">類型
+        /// <para>0:汽車</para>
+        /// <para>1:機車</para>
+        /// </param>
+        /// <returns></returns>
+        public List<MonthlyRentData> GetSubscriptionRates(string IDNO, string SD, string ED, int RateType)
+        {
+            bool flag = false;
+            List<ErrorInfo> lstError = new List<ErrorInfo>();
+            List<MonthlyRentData> lstMonthlyRent = null;
+            string SQL = "SELECT Mode,MonthlyRentId,IDNO,WorkDayHours,HolidayHours,MotoTotalHours,[WorkDayRateForCar],[HoildayRateForCar],[WorkDayRateForMoto],[HoildayRateForMoto],[StartDate],[EndDate]  FROM [TB_MonthlyRent] ";
+            SqlParameter[] para = new SqlParameter[4];
+            string term = "";
+            int nowCount = 0;
+            if (false == string.IsNullOrEmpty(IDNO))
+            {
+                term = " IDNO=@IDNO";
+                para[nowCount] = new SqlParameter("@IDNO", SqlDbType.VarChar, 30);
+                para[nowCount].Value = IDNO;
+                para[nowCount].Direction = ParameterDirection.Input;
+                nowCount++;
+            }
+
+            if (false == string.IsNullOrEmpty(SD) && false == string.IsNullOrEmpty(ED))
+            {
+                if (term != "")
+                {
+                    term += " AND ";
+                }
+                term += " ((@SD BETWEEN  StartDate AND EndDate) OR (@ED BETWEEN  StartDate AND EndDate))";
+                para[nowCount] = new SqlParameter("@SD", SqlDbType.VarChar, 30);
+                para[nowCount].Value = SD;
+                para[nowCount].Direction = ParameterDirection.Input;
+                nowCount++;
+                para[nowCount] = new SqlParameter("@ED", SqlDbType.VarChar, 30);
+                para[nowCount].Value = ED;
+                para[nowCount].Direction = ParameterDirection.Input;
+                nowCount++;
+            }
+            if (term != "")
+            {
+                term += " AND  Mode=@RateType";
+                para[nowCount] = new SqlParameter("@RateType", SqlDbType.TinyInt, 1);
+                para[nowCount].Value = RateType;
+                para[nowCount].Direction = ParameterDirection.Input;
+                nowCount++;
+            }
+
+
+            if ("" != term)
+            {
+                SQL += " WHERE " + term;
+            }
+            SQL += "  ORDER BY StartDate ASC";
+
+            lstMonthlyRent = GetObjList<MonthlyRentData>(ref flag, ref lstError, SQL, para, term);
+            return lstMonthlyRent;
+        }
+    }
+}
---
+++ iRentDBV2/dbo/Tables/TB_MonthlyRentHistory.sql
@@ -0,0 +1,59 @@
+﻿CREATE TABLE [dbo].[TB_MonthlyRentHistory]
+(
+	[HistoryID]         BIGINT       IDENTITY (1, 1) NOT NULL,
+    [OrderNo]          BIGINT          DEFAULT ((0)) NOT NULL,
+    [IDNO]              VARCHAR (20)   DEFAULT ('') NOT NULL,
+    [StartDate]         DATETIME     NOT NULL,
+    [EndDate]           DATETIME     NOT NULL,
+    [UseWorkDayHours]   FLOAT (53)   DEFAULT ((0)) NOT NULL,
+    [UseHolidayHours]   FLOAT (53)   DEFAULT ((0)) NOT NULL,
+    [UseMotoTotalHours] FLOAT (53)   DEFAULT ((0)) NOT NULL,
+    [isSend]            TINYINT      DEFAULT ((0)) NOT NULL,
+    [MonthlyRentId]    BIGINT       DEFAULT ((0)) NOT NULL,
+    [MKTime]            DATETIME    DEFAULT (DATEADD(HOUR,8,GETDATE())) NOT NULL,
+    CONSTRAINT [PK_TB_MonthlyRentHistory] PRIMARY KEY CLUSTERED ([HistoryID] ASC)
+)
+GO
+CREATE NONCLUSTERED INDEX [IX_TB_MonthlyRentHistory_SearchOrder]
+    ON [dbo].[TB_MonthlyRentHistory]([IDNO] ASC, [isSend] ASC, [OrderNo] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_TB_MonthlyRentHistory_SearchByPK]
+    ON [dbo].[TB_MonthlyRentHistory]([MonthlyRentId] ASC);
+    
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'建立時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'MKTime';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'0:未送出;1:已發送', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'isSend';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'機車總時數', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'UseMotoTotalHours';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'汽車假日時數', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'UseHolidayHours';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'汽車平日時數', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'UseWorkDayHours';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'訂閱迄', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'EndDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'訂閱起始', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'StartDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'身份證', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'IDNO';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'訂單編號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TB_MonthlyRentHistory', @level2type = N'COLUMN', @level2name = N'OrderNo';
+
