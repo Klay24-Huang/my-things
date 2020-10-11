34d340c02bdbaa0160fb62d87c04ed52ea363802
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-10-11 14:15:08 +0800

    邀請碼

---
+++ Domain/WebAPI/output/HiEasyRentAPI/WebAPIOutput_NPR320Query.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.WebAPI.output.HiEasyRentAPI
+{
+    public class WebAPIOutput_NPR320Query
+    {
+        /// <summary>
+        /// 回傳的訊息
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Data { get; set; }
+        /// <summary>
+        /// 執行結果
+        /// <para>true:成功</para>
+        /// <para>false:失敗</para>
+        /// </summary>
+        public bool Result { get; set; }
+        /// <summary>
+        ///回傳代碼
+        ///<para>0:成功</para>
+        /// </summary>
+        public string RtnCode { get; set; }
+    }
+}
