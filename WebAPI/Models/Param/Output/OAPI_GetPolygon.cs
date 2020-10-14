8da2d1302bf663199a006b9cefc7990ee8ffbfaa
adamliu6646 <Adam.Liu@mail.searching-service.com>
2020-10-14 18:31:42 +0800

    電子柵欄調整

--- WebAPI/Models/Param/Output/OAPI_GetPolygon.cs
+++ WebAPI/Models/Param/Output/OAPI_GetPolygon.cs
@@ -8,6 +8,15 @@ namespace WebAPI.Models.Param.Output
 {
     public class OAPI_GetPolygon
     {
-        public List<PolygonData> PolygonObj { set; get; }
+        //public List<PolygonData> PolygonObj { set; get; }
+        /// <summary>
+        /// 電子柵欄類型
+        /// <para>0:可還車區域</para>
+        /// <para>1:可還車區域中的不可還車區域</para>
+        /// <para>2:高風險區域</para>
+        /// <para>3:優惠區域</para>
+        /// </summary>
+        public int PolygonType { set; get; }
+        public string[] PolygonObj { set; get; }
     }
 }
