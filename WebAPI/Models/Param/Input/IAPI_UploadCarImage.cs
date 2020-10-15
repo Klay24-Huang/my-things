ffed9d32e37ca352db83f1b6511b239e76c05ec5
adamliu6646 <Adam.Liu@mail.searching-service.com>
2020-10-15 15:41:18 +0800

    上傳出還車照補檔

--- WebAPI/Models/Param/Input/IAPI_UploadCarImage.cs
+++ WebAPI/Models/Param/Input/IAPI_UploadCarImage.cs
@@ -18,6 +18,42 @@ namespace WebAPI.Models.Param.Input
         /// 1~8
         /// </summary>
         public int CarType { set; get; }
+
+        /// <summary>
+        /// 圖片base64
+        /// </summary>
+        public string CarImage { set; get; }
+
+    }
+
+    public class IAPI_UploadCarImage2
+    {
+        public string OrderNo { set; get; }
+        /// <summary>
+        /// 模式
+        /// <para>0:取車</para>
+        /// <para>1:還車</para>
+        /// </summary>
+        public int Mode { set; get; }
+        /// <summary>
+        /// 1~8
+        /// </summary>
+        //public int CarType { set; get; }
+
+        /// <summary>
+        /// 圖片base64
+        /// </summary>
+        //public string CarImage { set; get; }
+
+        public List<CarImages> CarImages { get; set; }
+    }
+
+    public class CarImages
+    {
+        /// <summary>
+        /// 1~8
+        /// </summary>
+        public int CarType { set; get; }
         /// <summary>
         /// 圖片base64
         /// </summary>
