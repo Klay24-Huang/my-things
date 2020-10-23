1735e65ca976e88baa19453d2d2c71697c2a5967
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-10-23 09:50:44 +0800

    加入合約

---
+++ Web/Controllers/ContactController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult returnCarNew(string OrderNum)
+        {
+            //CreateContractNew obj = new CreateContractNew();
+            //ContractOnlyRepository _repository = new ContractOnlyRepository(MvcApplication.connetStr);
+            //if (!string.IsNullOrEmpty(OrderNum))
+            //{
+            //    List<CreateContractNew> lstObj = _repository.GetContractData(OrderNum.Replace("H", ""));
+            //    if (lstObj != null)
+            //    {
+            //        if (lstObj.Count > 0)
+            //        {
+            //            obj = lstObj[0];
+            //        }
+            //    }
+            //}
+            return View();
+        }
+    }
+}
