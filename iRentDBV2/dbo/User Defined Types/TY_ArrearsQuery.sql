0e3d29ab6c867d4638a454b0bfb7c74d6812f16d
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-10-26 14:00:21 +0800

    上傳證件照寫入AZURE

---
+++ iRentDBV2/dbo/User Defined Types/TY_ArrearsQuery.sql
@@ -0,0 +1,17 @@
+﻿CREATE TYPE [dbo].[TY_ArrearsQuery] AS TABLE (
+    [CUSTID]       VARCHAR (10)   NULL,
+    [ORDNO]        VARCHAR (20)   NULL,
+    [CNTRNO]       VARCHAR (20)   NULL,
+    [PAYMENTTYPE]  VARCHAR (5)    NULL,
+    [SPAYMENTTYPE] NVARCHAR (100) NULL,
+    [DUEAMT]       INT            NULL,
+    [PAIDAMT]      INT            NULL,
+    [CARNO]        VARCHAR (30)   NULL,
+    [POLNO]        VARCHAR (50)   NULL,
+    [PAYTYPE]      VARCHAR (5)    NULL,
+    [GIVEDATE]     VARCHAR (16)   NULL,
+    [RNTDATE]      VARCHAR (16)   NULL,
+    [INBRNHCD]     VARCHAR (5)    NULL,
+    [IRENTORDNO]   VARCHAR (10)   NULL,
+    [TAMT]         INT            NULL);
+
