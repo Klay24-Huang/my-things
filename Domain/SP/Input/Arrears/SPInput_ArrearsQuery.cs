2e1b88d9b17337c5c72f8004afe033e94483377f
gogolucky168 <gogolucky168@gmail.com>
2020-10-27 12:02:27 +0800

    ArrearsQuery增加儲存功能

---
+++ Domain/SP/Input/Arrears/SPInput_ArrearsQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SP.Input.Arrears
+{
+    public class SPInput_ArrearsQuery
+    {
+        /// <summary>
+        /// 使否儲存查詢紀錄:0(不儲存), 1(儲存)
+        /// </summary>
+        public int IsSave { get; set; }
+        public Int64 LogID { get; set; }
+    }
+}
--- iRentDBV2/dbo/Stored Procedures/usp_ArrearsQuery_U1.sql
+++ iRentDBV2/dbo/Stored Procedures/usp_ArrearsQuery_U1.sql
@@ -6,6 +6,7 @@
 CREATE PROCEDURE [dbo].[usp_ArrearsQuery_U1]
 (   
 	@MSG					VARCHAR(10) OUTPUT,
+	@IsSave                 INT = 0           , --使否儲存查詢紀錄:0(不儲存), 1(儲存)
     @LogID                  BIGINT,
     @ArrearsQuery  TY_ArrearsQuery  READONLY
 )
@@ -25,11 +26,14 @@ BEGIN
 	BEGIN TRY
        /*
 		declare @t TY_ArrearsQuery 
+		declare @msg nvarchar(max)
 		insert into @t 
-		values('1','2','3','4','1',6,7,'8','9','1','11','12','13','14',15)
-		exec usp_ArrearsQuery_U1 1, @t	   
+		values('1','2','3','4','1',6,7,'8','9','10','11','12','13','14',15)
+		exec usp_ArrearsQuery_U1 @msg, 1,9999, @t	   
 	   */		
 	    
+       DROP TABLE IF EXISTS #tmp_ArrearsQuery
+
 	   IF @LogID IS NULL OR @LogID = ''
 	   BEGIN
 		   SET @Error=1
@@ -42,23 +46,56 @@ BEGIN
 			DECLARE @ArrearsQuery_count INT 
 			SELECT @ArrearsQuery_count = COUNT(*) FROM @ArrearsQuery
 			IF @ArrearsQuery_count > 0
-			BEGIN
+			BEGIN					
 					SELECT 
+					a.CARNO AS CarNo, --車號
 					a.TAMT AS Amount, --待繳金額
 					a.PAYMENTTYPE AS ArrearsKind, --欠費種類 1:租金,2:罰單,3:停車費,4:ETAG
 					a.RNTDATE AS EndDate, --實際還車時間
 					a.GIVEDATE AS StartDate, --實際取車時間
-					OrderNo = CASE WHEN a.IRENTORDNO IS NULL OR a.IRENTORDNO = '' THEN '-'
-					          ELSE 'H' + RIGHT(REPLICATE('0', 7) + CAST(a.IRENTORDNO as NVARCHAR), 7) END,
+					OrderNo = a.IRENTORDNO,					         
 					a.CNTRNO AS ShortOrderNo, --短租合約編號
-					StationName = ISNULL((SELECT TOP 1 s.StationName FROM TB_ManagerStation s WHERE s.StationID = a.INBRNHCD),'-'), --取車據點
+					a.INBRNHCD AS StationID, --取車據點
+					CarType = ( SELECT TOP 1 c.CarType FROM TB_Car c WHERE c.CarNo = a.CARNO), --車型代碼
+					IsMotor = (SELECT TOP 1 c.IsMotor FROM TB_CarInfo c WHERE c.CarNo = a.CARNO) ---是否是機車0否,1是
+					INTO #tmp_ArrearsQuery
+					FROM  @ArrearsQuery a	
+
+					IF @IsSave = 1
+					BEGIN
+					   INSERT INTO TB_NPR330  
+					   SELECT 
+					   t.CarNo,
+					   t.Amount,
+					   t.ArrearsKind,
+					   t.StartDate,
+					   t.EndDate,
+					   t.OrderNo,
+					   t.ShortOrderNo,
+					   t.StationID,
+					   t.CarType,
+					   t.IsMotor,
+					   GETDATE(),
+					   GETDATE()
+					   FROM #tmp_ArrearsQuery t
+					END
+
+					SELECT 
+					t.Amount, --待繳金額
+					t.ArrearsKind, --欠費種類 1:租金,2:罰單,3:停車費,4:ETAG
+					t.EndDate, --實際還車時間
+					t.StartDate, --實際取車時間
+					OrderNo = CASE WHEN t.OrderNo IS NULL OR t.OrderNo = '' THEN '-'
+					         ELSE 'H' + RIGHT(REPLICATE('0', 7) + CAST(t.OrderNo as NVARCHAR), 7) END,
+					t.ShortOrderNo, --短租合約編號
+					StationName = ISNULL((SELECT TOP 1 s.StationName FROM TB_ManagerStation s WHERE s.StationID = t.StationID),'-'), --取車據點
 					CarType = ISNULL(( --車型代碼
 						SELECT TOP 1 ct.CarTypeName FROM TB_CarType ct
 						WHERE ct.CarType = (
-							SELECT TOP 1 c.CarType FROM TB_Car c WHERE c.CarNo = a.CARNO)
+							SELECT TOP 1 c.CarType FROM TB_Car c WHERE c.CarNo = t.CarNo)
 					),''),
-					IsMotor = ISNULL((SELECT TOP 1 c.IsMotor FROM TB_CarInfo c WHERE c.CarNo = a.CARNO),0) ---是否是機車0否,1是
-					FROM  @ArrearsQuery a			    
+					IsMotor = ISNULL((SELECT TOP 1 c.IsMotor FROM TB_CarInfo c WHERE c.CarNo = t.CarNo),0) ---是否是機車0否,1是
+					FROM  #tmp_ArrearsQuery t
 			END
 			ELSE
 			BEGIN
@@ -84,6 +121,8 @@ BEGIN
         INSERT INTO TB_ErrorLog([FunName],[ErrorCode],[ErrType],[SQLErrorCode],[SQLErrorDesc],[LogID],[IsSystem])
         VALUES (@FunName,@ErrorCode,@ErrorType,@SQLExceptionCode,@SQLExceptionMsg,@LogID,@IsSystem);
 	END CATCH
+
+	DROP TABLE IF EXISTS #tmp_ArrearsQuery
 
 	--輸出系統訊息
 	SELECT @ErrorCode[ErrorCode], @ErrorMsg[ErrorMsg], @SQLExceptionCode[SQLExceptionCode], @SQLExceptionMsg[SQLExceptionMsg], @Error[Error]
