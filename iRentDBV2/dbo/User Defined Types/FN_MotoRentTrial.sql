6b9aa8a91986eb484d06022f25567b2ca8284a5d
gogolucky168 <gogolucky168@gmail.com>
2020-11-11 17:19:13 +0800

    機車租金試算-sp涵式

--- iRentDBV2/dbo/User Defined Types/FN_MotoRentTrial.sql
+++ iRentDBV2/dbo/User Defined Types/FN_MotoRentTrial.sql
@@ -1,7 +1,7 @@
 ﻿-- =============================================
 -- Author:      eason
--- Create Date: 2020-11-10
--- Description: 機車租金試算-不分平假日
+-- Create Date: 2020-11-11
+-- Description: 機車租金試算
 -- =============================================
 CREATE FUNCTION [dbo].[FN_MotoRentTrial]
 (
@@ -26,68 +26,99 @@ BEGIN
 
    declare @TotalMinutes int = DATEDIFF(minute, @SD, @ED) --總分鐘數
 
+   if @TotalMinutes = 0 --開始結束相同
+      return 0
+
    declare @BaseMinutes int = 0
-   declare @BasePrice float = 0
+   declare @BaseMinutesPrice float = 0
+   declare @BaseMinutesPriceH float = 0
    declare @Price float = 0 --平日價
    declare @PriceH float = 0 --假日價
    declare @MaxPrice int = 0 --平日上限
-   declare @MaxPriceH int = 0 --假日上限
+   declare @MaxPriceH int = 0 --假日上限   
 
    SELECT TOP 1 
 	@BaseMinutes = isnull(p.BaseMinutes,0),
-    @BasePrice = isnull(p.BaseMinutesPrice,0) * isnull(p.BaseMinutes,0), --低消
+    @BaseMinutesPrice = isnull(p.BaseMinutesPrice,0), 
+	@BaseMinutesPriceH = isnull(p.BaseMinutesPriceH,0),
 	@Price = isnull(p.Price, 0),
-	@PriceH = isnull(p.MaxPriceH, 0),
+	@PriceH = isnull(p.PriceH, 0),
 	@MaxPrice = isnull(p.MaxPrice, 0), --平日上限
 	@MaxPriceH = isnull(p.MaxPriceH, 0) --假日上限
    FROM TB_PriceByMinutes p
    WHERE p.ProjID = @ProjID and p.CarType = @CarType
-
-   if @Price = 0 or @MaxPrice = 0
+   and p.use_flag = 1
+   
+   if @BaseMinutes = 0 or @BaseMinutesPrice = 0 or @BaseMinutesPriceH = 0 or @Price = 0  or @PriceH = 0 or @MaxPrice = 0 or @MaxPriceH =0
        return -1 
 
+   declare @xdays int = DATEDIFF(day,@SD,@ED)
+   declare @strSD varchar(10) = convert(varchar,@SD,112)
+   declare @strED varchar(10) = convert(varchar,@ED,112)
+
+   --低於最小分鐘數
    if @TotalMinutes > 0 and @TotalMinutes <=  @BaseMinutes
-       set @TotalPrice = @BasePrice
-   else if @TotalMinutes >0
    begin
-       
-	   --fix logic
-       --declare @days int = DATEDIFF(day,@SD,@ED)
-       --declare @hours int = DATEDIFF(hour,@SD,@ED) % 24
-       --declare @mins int = @TotalMinutes % 60
+		declare @HolidayIndex int = 0
+		select top 1 @HolidayIndex = h.HolidayID from TB_Holiday h
+		where h.HolidayDate = @strSD 
+		and h.use_flag = 1		
 
-	   --原cs邏輯保留
-       declare @days int = 0
-       declare @hours int = 0
-       declare @mins int = 0
-	   set @days = convert(int, FLOOR(@TotalMinutes/600)) 
-	   set @hours = convert(int, FLOOR((@TotalMinutes % 600)/60))
-	   set @mins = @TotalMinutes - (@days * 600) - (@hours*60)
-
-	   declare @final_hour_cost float = 0 
-	   declare @hour_cost float = (@Price*60*@hours)
-
-	   if @hour_cost < @MaxPrice
-	   begin
-	      set @final_hour_cost = @hour_cost
-       end
-	   else
-          set @final_hour_cost = @MaxPrice
-
-	   set @TotalPrice = (@MaxPrice * @days) + @final_hour_cost + (@mins * @Price) + @BaseMinutes 
+		if @HolidayIndex > 0
+		  return @BaseMinutes * @BaseMinutesPriceH
+        else
+		  return @BaseMinutes * @BaseMinutesPrice       
    end
-   else
-       set @TotalPrice = 0 --時數是0
 
-   set @re = @TotalPrice
+   declare @xMins int = 0
+   if @xdays = 0 --當天
+   begin       
+      set @xMins = DATEDIFF(minute, @SD, @ED)
+	  set @re = dbo.FN_MotoRentCompute(@xMins,@Price,@PriceH,@MaxPrice,@MaxPriceH,@strSD)
+   end
+   else if @xdays = 1 --隔天
+   begin
+      declare @tmp_ed_star datetime = convert(datetime, (convert(varchar, @ED, 111)+ ' ' + '00:00:00'))
+	  declare @day_one_mins int = DATEDIFF(minute,@SD, @tmp_ed_star)
+	  declare @day_end_mins int = DATEDIFF(minute,@tmp_ed_star, @ED) 
+	  set @re += dbo.FN_MotoRentCompute(@day_one_mins, @Price, @PriceH, @MaxPrice, @MaxPriceH, @strSD) --首日花費
+	  set @re += dbo.FN_MotoRentCompute(@day_end_mins, @Price, @PriceH, @MaxPrice, @MaxPriceH, @strED) --尾日花費
+   end
+   else if @xdays > 1 
+   begin
+
+       declare @tm_sd_end datetime = convert(datetime, (convert(varchar, dateadd(day,1, @SD) , 111)+ ' ' + '00:00:00')) --首日結束
+  	   declare @tm_ed_star datetime = convert(datetime, (convert(varchar, @ED, 111)+ ' ' + '00:00:00')) --尾日開始
+
+	    declare @day_one_m int = DATEDIFF(minute,@SD, @tm_sd_end) --首日時間
+	    set @re += dbo.FN_MotoRentCompute(@day_one_m, @Price, @PriceH, @MaxPrice, @MaxPriceH, @strSD) --首日花費
+
+	    declare @day_end_m int = DATEDIFF(minute,@tm_ed_star,@ED)
+	    set @re += dbo.FN_MotoRentCompute(@day_end_m, @Price, @PriceH, @MaxPrice, @MaxPriceH, @strED) --尾日花費 
+
+		declare @day_star_count date = convert(date, @strSD) 
+        declare @day_end_count date = convert(date, @strED)
+		set @day_star_count = DATEADD(day,1,@day_star_count) --次日開始
+
+		while ( @day_star_count < @day_end_count)
+		begin				
+			declare @str_date varchar(20) = convert(varchar, @day_star_count, 112) 
+			set @re += dbo.FN_MotoRentCompute(24*60, @Price, @PriceH, @MaxPrice, @MaxPriceH, @str_date)
+			set @day_star_count  = DATEADD(day,1,@day_star_count)
+		end
+   end
+
+   set @re = FLOOR(@re)
 
    RETURN @re
 END
 
 /*
-    declare @SD datetime --預計取車時間
-	declare @ED datetime --預計還車時間
+    declare @SD datetime 
+	declare @ED datetime 
 	declare @ProjID varchar(10) 
 	declare @CarType varchar(10)
+
+	select dbo.FN_MotoRentTrial(@SD,@ED,@ProjID,@CarType)
 */
 GO
