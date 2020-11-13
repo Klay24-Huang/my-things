be9ba972aee52fddab670162d38581d82ce11c91
Jerry <jerry@mail.searching-service.com>
2020-11-13 15:57:43 +0800

    增加清除的處理

--- Web/Scripts/js/ActionJS/MemberManage/Audit.js
+++ Web/Scripts/js/ActionJS/MemberManage/Audit.js
@@ -11,6 +11,13 @@
     }
 
     setPostbackValue();
+
+    $('.btn-clear').click(function () {
+        setTimeout(function () {
+            $('#AuditMode').val('');
+            $('.form-check-input').attr('checked', false);
+        },300);
+    });
 });
 function GoDetail(IDNO) {
     ShowLoading("資料讀取車…");
