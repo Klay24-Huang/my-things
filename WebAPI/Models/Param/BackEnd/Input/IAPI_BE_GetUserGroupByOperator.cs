47afab4038fa83d2b25e4c1d278a68ad5b5d1955
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-11-13 17:06:52 +0800

    使用者維護finish

---
+++ WebAPI/Models/Param/BackEnd/Input/IAPI_BE_GetUserGroupByOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.BackEnd.Input
+{
+    public class IAPI_BE_GetUserGroupByOperator:IAPI_BE_Base
+    {
+        public int OperatorID { set; get; }
+        public int NowID { set; get; }
+        public int UserGroupID { set; get; }
+    }
+}
