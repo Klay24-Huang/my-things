88b677bd79edfa9fd4ce071cf05a2346ea7bc86d
Jerry <jerry@mail.searching-service.com>
2020-11-13 18:10:00 +0800

    修正會員審核圖片存檔及查詢相關處理

--- Web/Scripts/js/ActionJS/MemberManage/Audit.js
+++ Web/Scripts/js/ActionJS/MemberManage/Audit.js
@@ -18,6 +18,11 @@
             $('.form-check-input').attr('checked', false);
         },200);
     });
+    setTimeout(function () {
+        if ($.trim($('AuditMode').val()) != '') {
+            $('.btn-submit').click();
+        }
+    }, 500);
 });
 function GoDetail(IDNO) {
     ShowLoading("資料讀取車…");
