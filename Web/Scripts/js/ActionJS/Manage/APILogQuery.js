c714b3ae4f844513789f4bd0622490fb01569207
Jerry <jerry@mail.searching-service.com>
2020-11-17 04:15:00 +0800

    增加APILogQuery

---
+++ Web/Scripts/js/ActionJS/Manage/APILogQuery.js
@@ -0,0 +1,11 @@
+﻿var Operator = "";
+$(document).ready(function () {
+
+    var hasData = parseInt($("#len").val());
+    console.log(hasData);
+    if (hasData > 0) {
+        $('.table').footable();
+    }
+  
+})
+
