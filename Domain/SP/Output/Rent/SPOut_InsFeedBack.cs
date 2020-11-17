88cd4948b014681210f290d96604940f8ff7e473
Jet_Yeh <good79324@gmail.com>
2020-11-17 09:45:30 +0800

    上傳回饋照片：補SPOut檔

---
+++ Domain/SP/Output/Rent/SPOut_InsFeedBack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Domain.SP.Output.Rent
+{
+    public class SPOut_InsFeedBack
+    {
+        /// <summary>
+        /// 照片1檔案名稱
+        /// </summary>
+        public string PIC1 { get; set; }
+
+        /// <summary>
+        /// 照片2檔案名稱
+        /// </summary>
+        public string PIC2 { get; set; }
+
+        /// <summary>
+        /// 照片3檔案名稱
+        /// </summary>
+        public string PIC3 { get; set; }
+
+        /// <summary>
+        /// 照片4檔案名稱
+        /// </summary>
+        public string PIC4 { get; set; }
+    }
+}
