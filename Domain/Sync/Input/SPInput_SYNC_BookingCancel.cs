1b43ee6597f6a3b685957740028d9dfdeab1b9af
DESKTOP-AACU8CQ\Eric <ericguo0930@gmail.com>
2020-12-09 06:02:21 +0800

    逾時取消暨接近還車前通知排程

---
+++ Domain/Sync/Input/SPInput_SYNC_BookingCancel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Sync.Input
+{
+    public class SPInput_SYNC_BookingCancel
+    {
+        public DateTime NowTime { set; get; }
+    }
+}
