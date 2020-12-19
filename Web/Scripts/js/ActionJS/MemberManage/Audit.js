043e933c45b59e2ed09b8a870a49ca9b5dce3ab3
Jerry <jerry@mail.searching-service.com>
2020-12-20 07:27:02 +0800

    增加查詢條件檢核

--- Web/Scripts/js/ActionJS/MemberManage/Audit.js
+++ Web/Scripts/js/ActionJS/MemberManage/Audit.js
@@ -23,6 +23,48 @@
             $('.btn-submit').click();
         }
     }, 500);
+    var StartDate = new Date();
+    var EndDate = new Date();
+    StartDate.setDate(StartDate.getDate() -10);
+    $('#idEndNo .form-check-input').click(function () {
+        if ($('#StartDate').val() == '') {
+            $("#StartDate").val($.format.date(StartDate, 'yyyy-MM-dd'));
+        }
+        if ($('#EndDate').val() == '') {
+            $("#EndDate").val($.format.date(EndDate, 'yyyy-MM-dd'));
+        }
+        if ($('#AuditType').val() == '' || $('#AuditType').val() == '-1') {
+            $('#AuditType').val('0');
+        }
+    });
+
+    if ($('#StartDate').val() == '') {
+        $("#StartDate").val($.format.date(StartDate, 'yyyy-MM-dd'));
+    }
+    if ($('#EndDate').val() == '') {
+        $("#EndDate").val($.format.date(EndDate, 'yyyy-MM-dd'));
+    }
+    if ($('#AuditType').val() == '' || $('#AuditType').val() == '-1') {
+        $('#AuditType').val('0');
+    }
+
+    $('.IDNO').change(function () {
+        if ($(this).val().length >= 2) {
+            $("#StartDate").val('');
+            $("#EndDate").val('');
+            $('#AuditType').val('');
+        } else {
+            if ($('#StartDate').val() == '') {
+                $("#StartDate").val($.format.date(StartDate, 'yyyy-MM-dd'));
+            }
+            if ($('#EndDate').val() == '') {
+                $("#EndDate").val($.format.date(EndDate, 'yyyy-MM-dd'));
+            }
+
+        }
+    });
+
+
 });
 function GoDetail(IDNO) {
     ShowLoading("資料讀取中…");
