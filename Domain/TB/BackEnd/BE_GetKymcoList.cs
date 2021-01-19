d877a0cdae2e994f9ca0bdba23c79956294cc237
BensonTang <benson922@hotaimotor.com.tw>
2021-01-19 18:00:21 +0800

    新增光陽維運報表查詢2

---
+++ Domain/TB/BackEnd/BE_GetKymcoList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TB.BackEnd
+{
+    public class BE_GetKymcoList
+    {
+        /// <summary>
+        /// 員工代號
+        /// </summary>
+        public string UserID { set; get; }
+        /// <summary>
+        /// 員工姓名
+        /// </summary>
+        public string UserName { set; get; }
+        /// <summary>
+        /// 區域
+        /// </summary>
+        public string Area { set; get; }
+        /// <summary>
+        /// 種類
+        /// </summary>
+        public string TypeK { set; get; }
+        /// <summary>
+        /// 車號
+        /// </summary>
+        public string CarNo { set; get; }
+        /// <summary>
+        /// 經銷商 或 整備項目
+        /// </summary>
+        public string DealerCodeValue { set; get; }
+        /// <summary>
+        /// 備註 或 地址
+        /// </summary>
+        public string MemoAddr { set; get; }
+        /// <summary>
+        /// 維修方 (整備項目才有)
+        /// </summary>
+        public string MaintainType { set; get; }
+        /// <summary>
+        /// 原因次分類 (整備項目才有)
+        /// </summary>
+        public string Reason { set; get; }
+        /// <summary>
+        /// 車輛是否下線 (整備項目才有)
+        /// </summary>
+        public string Offline { set; get; }
+        /// <summary>
+        /// 修改時間
+        /// </summary>
+        public string UpdTime { set; get; }
+    }
+}
---
+++ Web/Scripts/js/ActionJS/Report/KymcoQuery.js
@@ -0,0 +1,51 @@
+﻿$(function () {
+    /*
+    setTimeout(function () {
+        if ($.trim($('AuditMode').val()) != '') {
+            $('.btn-submit').click();
+        }
+    }, 500);
+    */
+
+    $("#btnExplode").on("click", function () {
+        ShowLoading("資料查詢中…");
+        var flag = true;
+        var errMsg = "";
+        var SD = $("#StartDate").val();
+        var ED = $("#EndDate").val();
+        if (SD !== "" && ED !== "") {
+            if (SD > ED) {
+                flag = false;
+                errMsg = "起始日期大於結束日期";
+            } else {
+                var GetDateDiff = DateDiff(SD, ED);
+                if (GetDateDiff > 3) {
+                    flag = false;
+                    errMsg = "時間區間大於3天";
+                }
+            }
+        } else {
+            flag = false;
+            errMsg = "未選擇日期";
+        }
+        if (flag) {
+            $("#ExplodeSDate").val($("#StartDate").val());
+            $("#ExplodeEDate").val($("#EndDate").val());
+            $("#ExplodeAuditMode").val($("#AuditMode").val());
+            disabledLoading();
+            $("#frmKymcoExplode").submit();
+        } else {
+            disabledLoadingAndShowAlert(errMsg);
+            //ShowFailMessage(errMsg);
+        }
+    });
+});
+var DateDiff = function (sDate1, sDate2) { // sDate1 和 sDate2 是 2016-06-18 格式
+    var aDate, oDate1, oDate2, iDays
+    aDate = sDate1.split("/")
+    oDate1 = new Date(aDate[1] + '/' + aDate[2] + '/' + aDate[0]) // 轉換為 06/18/2016 格式
+    aDate = sDate2.split("/")
+    oDate2 = new Date(aDate[1] + '/' + aDate[2] + '/' + aDate[0])
+    iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24) // 把相差的毫秒數轉換為天數
+    return iDays;
+};
