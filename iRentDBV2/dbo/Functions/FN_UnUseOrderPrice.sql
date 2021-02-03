280395af41d9d4800bab27e56d29ed9b931f0ed4
gogolucky168 <gogolucky168@gmail.com>
2021-02-03 09:09:14 +0800

    租金相關sp同步

--- iRentDBV2/dbo/Functions/FN_UnUseOrderPrice.sql
+++ iRentDBV2/dbo/Functions/FN_UnUseOrderPrice.sql
@@ -15,7 +15,7 @@ RETURNS float
 AS
 BEGIN
    declare @re float = 0
-   if @OrderPrice > 0 and @BookingSD <= @BookingED and @FinalSD <= @FinalED and @FinalSD >= @BookingSD
+   if @OrderPrice > 0 and @BookingSD <= @BookingED and @FinalSD <= @FinalED 
    begin      
 	   set @BookingSD = DATEADD(second, DATEPART(SECOND, @BookingSD)*-1,@BookingSD)--去秒數
 	   set @BookingED = DATEADD(second, DATEPART(SECOND, @BookingED)*-1,@BookingED)--去秒數
@@ -31,29 +31,24 @@ BEGIN
 
 	   declare @ck_days int = 0
 	   declare @ck_mins int = 0
-	   --@BookingED與@FinalED的絕對值計算比對天數
 	   if @FinalED <= @BookingED
 	   begin	     
 		  set @ck_mins = datediff(minute,@finalED, @BookingED) 
 		  set @ck_days = floor(cast(@ck_mins as float)/(24*60))
        end
 	   else
-	   begin
-		   set @ck_mins =  datediff(minute, @BookingED,@finalED)
-		   set @ck_days = floor(cast(@ck_mins as float)/(24*60))
-	   end
+          return 0
+
 	   declare @ck_xmins int = @ck_mins  % (24*60)    
 
 	   if @ck_mins < (24*60)
 	   begin
-	      --ED前後24HR內(不包含)免罰
-	      set @re = @OrderPrice
+	      --ED前24HR內(不包含)免罰
+	      set @re = 0
 	   end
 	   else
-	   begin		 
-		   if @ck_xmins > 0
-			  set @ck_days += 1	      		  
-		   declare @unUseDays float = @bok_days - @ck_days
+	   begin		       		  
+		   declare @unUseDays float = @ck_days
 		   set @unUseDays = iif(@unUseDays>0,@unUseDays,0)
 		   set @re = @unUseDays * @orderDayPice --未使用天數訂金
 		   set @re = iif(@re > 0,@re,0)
@@ -68,5 +63,7 @@ declare @BookingED datetime = dateadd(day,3,@BookingSD)
 declare @FinalSD datetime = dateadd(minute,-120,@BookingED)
 declare @FinalED datetime = dateadd(hour,3,@FinalSD)
 select dbo.FN_UnUseOrderPrice(3000,@BookingSD,@BookingED,@FinalSD,@FinalED)
+select @bok_mins[bok_mins], @bok_days[bok_days], @bok_xmins[bok_xmins]
+select @ck_mins[ck_mins], @ck_days[ck_days], @ck_xmins[ck_xmins]
 */
 GO
