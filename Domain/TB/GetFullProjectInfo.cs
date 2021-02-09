0579e40e39ab6ce2d30e3ed925c6a516df84bdb2
Jet_Yeh <good79324@gmail.com>
2021-02-09 13:24:12 +0800

    訂單列表：修正春節期間的路邊的平日/假日價格、機車的每日上限

---
+++ Domain/TB/GetFullProjectInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Domain.TB
+{
+    public class GetFullProjectInfo
+    {
+        /// <summary>
+        /// 優惠專案代碼
+        /// </summary>
+        public string PROJID { get; set; }
+
+        /// <summary>
+        /// 專案起日
+        /// </summary>
+        public DateTime ShowStart { get; set; }
+
+        /// <summary>
+        /// 專案迄日
+        /// </summary>
+        public DateTime ShowEnd { get; set; }
+
+        /// <summary>
+        /// 專案類型
+        /// </summary>
+        public string PROJTYPE { get; set; }
+
+        /// <summary>
+        /// 據點ID
+        /// </summary>
+        public string StationID { get; set; }
+
+        /// <summary>
+        /// 車型
+        /// </summary>
+        public string CARTYPE { get; set; }
+
+        /// <summary>
+        /// 優惠價(平日)
+        /// </summary>
+        public int PRICE { get; set; }
+
+        /// <summary>
+        /// 優惠價(假日)
+        /// </summary>
+        public int PRICE_H { get; set; }
+
+        /// <summary>
+        /// 車型名稱
+        /// </summary>
+        public string CarTypeName { get; set; }
+    }
+}
