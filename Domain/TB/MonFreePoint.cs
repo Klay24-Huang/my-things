f0d72b4ca2784e96b16cb01612da145a394ff4e9
gogolucky168 <gogolucky168@gmail.com>
2021-02-25 11:06:06 +0800

    月租免費點數VM

---
+++ Domain/TB/MonFreePoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TB
+{
+    public class MonFreePoint
+    {
+        public Int64 MonFreePointId { get; set; }
+        public Int64 MonthlyRentId { get; set; }
+        public int MotoFreeType { get; set; }
+        public double MotoTotalFreeMins { get; set; }
+        public double MotoWorkDayFreeMins { get; set; }
+        public double MotoHolidyDayFreeMins { get; set; }
+        public int CarFreeType { get; set; }
+        public double CarTotalFreeHours { get; set; }
+        public double CarWorkDayFreeHours { get; set; }
+        public double CarHolidayFreeHours { get; set; }
+    }
+}
