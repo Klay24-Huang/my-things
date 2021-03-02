757a0df0c19ff49cb7c49cbe4d45b77a508181d7
Jet_Yeh <good79324@gmail.com>
2021-03-02 16:26:04 +0800

    月租訂閱：機車的優惠費率由input傳入，汽車才給預設值

--- iRentDBV2/dbo/Stored Procedures/usp_MonthRent_I01.sql
+++ iRentDBV2/dbo/Stored Procedures/usp_MonthRent_I01.sql
@@ -14,7 +14,7 @@
 ** Date:     |   Author:  |          Description:
 ** ----------|------------| ------------------------------------
 ** 2020/10/31|  ADAM      |    First Release
-**			 |			  |
+** 2021/02/05 UPD BY Brian 調整匯入月租判斷條件抓取SEQNO
 *****************************************************************/
 /*
 EXAMPLE
@@ -72,97 +72,92 @@ SET @hasData=0;
 SET @Mode=0
 SET @NowTime=DATEADD(hour,8,GETDATE());
 
-	BEGIN TRY
-
-
+BEGIN TRY
 		--IF @MotorTotalHours > 0
 		--BEGIN
 		--	SET @Mode=1
 		--END
 
-		SELECT @hasData=COUNT(1) FROM TB_MonthlyRent A WITH(NOLOCK)  
-		JOIN @MonthlyRent B ON A.IDNO=B.IDNO 
-			AND A.Mode=CASE WHEN B.MotorTotalHours > 0 THEN 1 ELSE 0 END
-			AND B.StartDate BETWEEN A.StartDate AND A.EndDate
-		
-		
-		IF @hasData>0
-		BEGIN
-			SET @Error=1
-			SET @ErrorCode='ERR227';
-		END
+	SELECT @hasData=COUNT(1) FROM TB_MonthlyRent A WITH(NOLOCK)  
+	JOIN @MonthlyRent B ON A.IDNO=B.IDNO 
+	AND A.Mode=CASE WHEN B.MotorTotalHours > 0 THEN 1 ELSE 0 END
+	--AND B.StartDate BETWEEN A.StartDate AND A.EndDate
+	AND A.SEQNO = B.SEQNO	--2021/02/05 UPD BY Brian 調整匯入月租判斷條件抓取SEQNO
 
-		IF @Error=0
-		BEGIN
-			INSERT INTO TB_MonthlyRent
-			(
-				ProjID, ProjNM, SEQNO, Mode, IDNO, WorkDayHours,
-				HolidayHours, MotoTotalHours, StartDate, EndDate,
-				MKTime, UPDTime
-			)
-			/*
-			VALUES
-			(
-				@ProjID, @ProjNM, @SEQNO, @Mode, @IDNO, @WorkDayHours,
-				@HolidayHours, @MotorTotalHours, @StartDate, @EndDate,
-				@NowTime, @NowTime
-			)*/
-			SELECT ProjID, ProjNM, SEQNO, CASE WHEN MotorTotalHours > 0 THEN 1 ELSE 0 END, IDNO, WorkDayHours,
-				HolidayHours, MotorTotalHours, StartDate, EndDate,
-				@NowTime, @NowTime
-			FROM @MonthlyRent
+	IF @hasData>0
+	BEGIN
+		SET @Error=1
+		SET @ErrorCode='ERR227';
+	END
 
-		END
+	IF @Error=0
+	BEGIN
+		INSERT INTO TB_MonthlyRent
+		(
+			ProjID, ProjNM, SEQNO, Mode, IDNO, WorkDayHours,
+			HolidayHours, MotoTotalHours, StartDate, EndDate,
+			WorkDayRateForMoto, HoildayRateForMoto,
+			MKTime, UPDTime
+		)
+		/*
+		VALUES
+		(
+			@ProjID, @ProjNM, @SEQNO, @Mode, @IDNO, @WorkDayHours,
+			@HolidayHours, @MotorTotalHours, @StartDate, @EndDate,
+			@NowTime, @NowTime
+		)*/
+		SELECT ProjID, ProjNM, SEQNO, CASE WHEN MotorTotalHours > 0 THEN 1 ELSE 0 END, IDNO, WorkDayHours,
+			HolidayHours, MotorTotalHours, StartDate, EndDate,
+			CASE WHEN MotorTotalHours > 0 THEN FavHFee ELSE 1.5 END, CASE WHEN MotorTotalHours > 0 THEN FavHFee ELSE 1.5 END,
+			@NowTime, @NowTime
+		FROM @MonthlyRent
+	END
 
-		--增加錯誤回傳
-		SELECT 
-			Error=@Error,
-			ErrorCode=@ErrorCode,
-			ErrorMsg=@ErrorMsg,
-			SQLExceptionCode=@SQLExceptionCode,
-			SQLExceptionMsg=@SQLExceptionMsg;
-	END TRY
-	BEGIN CATCH
-		SET @Error=-1;
-			SET @ErrorCode='ERR999';
-			SET @ErrorMsg='我要寫錯誤訊息';
-			SET @SQLExceptionCode=ERROR_NUMBER();
-			SET @SQLExceptionMsg=ERROR_MESSAGE();
+	--增加錯誤回傳
+	SELECT 
+		Error=@Error,
+		ErrorCode=@ErrorCode,
+		ErrorMsg=@ErrorMsg,
+		SQLExceptionCode=@SQLExceptionCode,
+		SQLExceptionMsg=@SQLExceptionMsg;
+END TRY
+BEGIN CATCH
+	SET @Error=-1;
+	SET @ErrorCode='ERR999';
+	SET @ErrorMsg='我要寫錯誤訊息';
+	SET @SQLExceptionCode=ERROR_NUMBER();
+	SET @SQLExceptionMsg=ERROR_MESSAGE();
 			
-			--增加錯誤回傳
-			SELECT 
-				Error=@Error,
-				ErrorCode=@ErrorCode,
-				ErrorMsg=@ErrorMsg,
-				SQLExceptionCode=@SQLExceptionCode,
-				SQLExceptionMsg=@SQLExceptionMsg;
+	--增加錯誤回傳
+	SELECT 
+		Error=@Error,
+		ErrorCode=@ErrorCode,
+		ErrorMsg=@ErrorMsg,
+		SQLExceptionCode=@SQLExceptionCode,
+		SQLExceptionMsg=@SQLExceptionMsg;
 
-			--IF @@TRANCOUNT > 0
-			--BEGIN
-			--	print 'rolling back transaction' /* <- this is never printed */
-			--	ROLLBACK TRAN
-			--END
-			 SET @IsSystem=1;
-			 SET @ErrorType=4;
-			      INSERT INTO TB_ErrorLog([FunName],[ErrorCode],[ErrType],[SQLErrorCode],[SQLErrorDesc],[LogID],[IsSystem])
-				 VALUES (@FunName,@ErrorCode,@ErrorType,@SQLExceptionCode,@SQLExceptionMsg,@LogID,@IsSystem);
-	END CATCH
+	--IF @@TRANCOUNT > 0
+	--BEGIN
+	--	print 'rolling back transaction' /* <- this is never printed */
+	--	ROLLBACK TRAN
+	--END
+	SET @IsSystem=1;
+	SET @ErrorType=4;
+	INSERT INTO TB_ErrorLog([FunName],[ErrorCode],[ErrType],[SQLErrorCode],[SQLErrorDesc],[LogID],[IsSystem])
+	VALUES (@FunName,@ErrorCode,@ErrorType,@SQLExceptionCode,@SQLExceptionMsg,@LogID,@IsSystem);
+END CATCH
 RETURN 0
 
 EXECUTE sp_addextendedproperty @name = N'Platform', @value = N'API', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_MonthRent_I01';
-
-
 GO
+
 EXECUTE sp_addextendedproperty @name = N'Owner', @value = N'Adam', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_MonthRent_I01';
-
-
 GO
+
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'月租訂閱', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_MonthRent_I01';
-
-
 GO
+
 EXECUTE sp_addextendedproperty @name = N'IsActive', @value = N'1:使用', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_MonthRent_I01';
-
-
 GO
+
 EXECUTE sp_addextendedproperty @name = N'Comments', @value = N'', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_MonthRent_I01';
