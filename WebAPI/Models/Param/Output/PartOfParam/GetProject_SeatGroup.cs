c4d59841de195132f2d6c4bbb0e95a5304fbf5fd
gogolucky168 <gogolucky168@gmail.com>
2021-03-09 16:20:53 +0800

    地圖搜尋

---
+++ WebAPI/Models/Param/Output/PartOfParam/GetProject_SeatGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Output.PartOfParam
+{
+    public class GetProject_SeatGroup
+    {
+        public Int16 Seat { get; set; }
+        public List<GetProject_CarInfo> CarInfos { get; set; } = new List<GetProject_CarInfo>(); 
+    }
+
+    public class GetProject_CarInfo
+    {
+        public Int16 Seat { get; set; }
+        public string CarTypes { get; set; }
+    }
+
+}
