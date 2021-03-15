6d8ed7ba4ec7dfe63867f6faf366f46571ef7354
adamliu6646 <Adam.Liu@mail.searching-service.com>
2021-03-15 10:25:20 +0800

    常用站點修改

--- WebAPI/Models/Param/Input/IAPI_SetFavoriteStation.cs
+++ WebAPI/Models/Param/Input/IAPI_SetFavoriteStation.cs
@@ -8,6 +8,24 @@ namespace WebAPI.Models.Param.Input
     public class IAPI_SetFavoriteStation
     {
         /// <summary>
+        /// 改為LIST方式  20210315 ADD BY ADAM 
+        /// </summary>
+        public List<FavoriteStation> FavoriteStations { get; set; }
+        /// <summary>
+        /// 據點代碼
+        /// </summary>
+        public string StationID { set; get; }
+        /// <summary>
+        /// 模式
+        /// <para>0:移除</para>
+        /// <para>1:設定</para>
+        /// </summary>
+        public Int16 Mode { set; get; }
+    }
+
+    public class FavoriteStation
+    {
+        /// <summary>
         /// 據點代碼
         /// </summary>
         public string StationID { set; get; }
