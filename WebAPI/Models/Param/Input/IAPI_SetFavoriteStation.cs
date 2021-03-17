a813b92c2d1fffd18140bdbeca5d633002009b16
gogolucky168 <gogolucky168@gmail.com>
2021-03-17 14:42:42 +0800

    設定最愛站點

--- WebAPI/Models/Param/Input/IAPI_SetFavoriteStation.cs
+++ WebAPI/Models/Param/Input/IAPI_SetFavoriteStation.cs
@@ -8,19 +8,9 @@ namespace WebAPI.Models.Param.Input
     public class IAPI_SetFavoriteStation
     {
         /// <summary>
-        /// 改為LIST方式  20210315 ADD BY ADAM 
+        /// 設定最愛站點
         /// </summary>
         public List<FavoriteStation> FavoriteStations { get; set; }
-        /// <summary>
-        /// 據點代碼
-        /// </summary>
-        public string StationID { set; get; }
-        /// <summary>
-        /// 模式
-        /// <para>0:移除</para>
-        /// <para>1:設定</para>
-        /// </summary>
-        public Int16 Mode { set; get; }
     }
 
     public class FavoriteStation
