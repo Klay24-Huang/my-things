949421efe630d2be15c391be5323a680f062c029
BensonTang <benson922@hotaimotor.com.tw>
2021-03-25 11:14:13 +0800

    會員審核新增個資小視窗

--- Web/Controllers/MemberManageController.cs
+++ Web/Controllers/MemberManageController.cs
@@ -90,7 +90,7 @@ namespace Web.Controllers
             }
 
             BE_AuditDetail obj = new MemberRepository(connetStr).GetAuditDetail(AuditIDNO);
-            List<BE_AuditImage> lstAudits=new MemberRepository(connetStr).GetAuditImage(AuditIDNO);
+            List<BE_AuditImage> lstAudits=new MemberRepository(connetStr).GetAuditImage(AuditIDNO);//抓出每個人的12張圖片資訊
             List<BE_AuditHistory> lstHistory = new MemberRepository(connetStr).GetAuditHistory(AuditIDNO);
             List<BE_InsuranceData> lstInsuranceData = new MemberRepository(connetStr).GetGetInsuranceData(AuditIDNO);
             List<BE_SameMobileData> lstMobile = null;
@@ -138,11 +138,13 @@ namespace Web.Controllers
             Data.detail = obj;
             Data.detail.SPED = obj.SPED != "" ? obj.SPED.Substring(0, 4) + "-" + obj.SPED.Substring(4, 2) + "-" + obj.SPED.Substring(6, 2) : obj.SPED;
             Data.detail.SPSD = obj.SPSD != "" ? obj.SPSD.Substring(0, 4) + "-" + obj.SPSD.Substring(4, 2) + "-" + obj.SPSD.Substring(6, 2) : obj.SPSD;
-            if (lstAudits != null)
+
+            if (lstAudits != null)//若有抓出圖片資訊就跑下面這段
             {
-                Data.Images = new BE_AuditCrentials();
+                Data.Images = new BE_AuditCrentials(); //宣告每張照片的屬性
                 for(int i = 1; i <= 11; i++)
                 {
+                    //判斷CrentialsType有值的就設定
                     int index = lstAudits.FindIndex(delegate (BE_AuditImage image)
                     {
                         return image.CrentialsType == i;
@@ -151,11 +153,13 @@ namespace Web.Controllers
                     {
                         switch (i)
                         {
+                            //用UPDTime去交叉判斷，到底為何要這麼麻煩?
+                            //若UPDTime是空(表示tmp沒資料)，則AuditResult=sp傳回的AuditResult。若UPDTime不是空，則AuditResult=(若sp傳回的AuditResult不是1且有註記失敗原因則=-1，不然就0)
+                            //20210325唐改，所有UPDTime判斷拿掉，信任SP丟回來的值
                             case 1:
                                 Data.Images.ID_1 = (lstAudits[index].CrentialsFile == "") ? "" : (lstAudits[index].CrentialsFile.ToUpper().IndexOf("FTP")>-1 ? lsURL : ImgURL) + lstAudits[index].CrentialsFile;
                                 Data.Images.ID_1_IsNew = 1;
                                 Data.Images.ID_1_UPDTime = lstAudits[index].UPDTime.ToString("yyyyMMdd") == "00010101" ? "" : "上傳時間：" + lstAudits[index].UPDTime.ToString("yyyy/MM/dd HH:mm:ss");
-                                //若UPDTime是空，則AuditResult=sp傳回的AuditResult。若UPDTime不是空，則AuditResult=(若sp傳回的AuditResult不是1且有註記失敗原因則=-1，不然就0)
                                 Data.Images.ID_1_AuditResult = lstAudits[index].UPDTime.ToString("yyyyMMdd") != "00010101" ? (lstAudits[index].AuditResult != 1 && lstAudits[index].RejectReason != "" ? -1 : 0) : lstAudits[index].AuditResult;
                                 Data.Images.ID_1_RejectReason = lstAudits[index].AuditResult == 1 ? "" : lstAudits[index].RejectReason;
                                 break;
@@ -235,7 +239,8 @@ namespace Web.Controllers
                     }
                     else
                     {
-                        int Oindex= lstAudits.FindIndex(delegate (BE_AuditImage image)
+                        //判斷AlreadyType有值的就設定
+                        int Oindex = lstAudits.FindIndex(delegate (BE_AuditImage image)
                         {
                             return image.AlreadyType == i;
                         });
@@ -321,9 +326,7 @@ namespace Web.Controllers
                                     Data.Images.Signture_1_RejectReason = lstAudits[Oindex].AuditResult == 1 ? "" : lstAudits[Oindex].RejectReason;
                                     break;
                             }
-                        }
-                       
-                      
+                        }            
                     }
                 }
             }
