5835dd142a1ce43b531db77b15fdb00c561e3cbe
Jet_Yeh <good79324@gmail.com>
2021-03-30 17:20:37 +0800

    告警機制建立

--- iRentDBV2/dbo/Tables/TB_AlertMailLog.sql
+++ iRentDBV2/dbo/Tables/TB_AlertMailLog.sql
@@ -1,78 +1,40 @@
-﻿CREATE TABLE [dbo].[TB_AlertMailLog]
+/****** Object:  Table [dbo].[TB_AlertMailLog]    Script Date: 2021/3/30 下午 05:14:00 ******/
+
+CREATE TABLE [dbo].[TB_AlertMailLog](
+	[AlertID] [bigint] IDENTITY(1,1) NOT NULL,
+	[EventType] [int] NOT NULL,
+	[Receiver] [varchar](1024) NOT NULL,
+	[Sender] [varchar](256) NOT NULL,
+	[HasSend] [tinyint] NOT NULL,
+	[CarNo] [varchar](20) NOT NULL,
+	[SendTime] [datetime] NULL,
+	[MKTime] [datetime] NOT NULL,
+	[UPDTime] [datetime] NULL,
+ CONSTRAINT [PK_TB_AlertMailLog] PRIMARY KEY CLUSTERED 
 (
-	[AlertID] BIGINT NOT NULL, 
-    [EventType] INT NOT NULL DEFAULT 0, 
-    [Receiver] VARCHAR(1024) NOT NULL DEFAULT '', 
-    [Sender] VARCHAR(256) NOT NULL DEFAULT '', 
-    [HasSend] TINYINT NOT NULL DEFAULT 0, 
-    [MKTime] DATETIME NOT NULL DEFAULT DATEADD(HOUR,8,GETDATE()), 
-    [UPDTime] DATETIME NULL 
-)
-
+	[AlertID] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
 
-CREATE INDEX [IX_TB_AlertMailLog_Search] ON [dbo].[TB_AlertMailLog] ([HasSend], [EventType], [MKTime])
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF__TB_AlertM__Event__4E00329D]  DEFAULT ((0)) FOR [EventType]
+GO
 
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF__TB_AlertM__Recei__4EF456D6]  DEFAULT ('') FOR [Receiver]
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'告警信',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = NULL,
-    @level2name = NULL
+
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF__TB_AlertM__Sende__4FE87B0F]  DEFAULT ('') FOR [Sender]
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'告警類別',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'EventType'
+
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF__TB_AlertM__HasSe__50DC9F48]  DEFAULT ((0)) FOR [HasSend]
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'收件者',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'Receiver'
+
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF_TB_AlertMailLog_CarNo]  DEFAULT ('') FOR [CarNo]
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'寄件者',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'Sender'
+
+ALTER TABLE [dbo].[TB_AlertMailLog] ADD  CONSTRAINT [DF__TB_AlertM__MKTim__51D0C381]  DEFAULT (dateadd(hour,(8),getdate())) FOR [MKTime]
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'是否發送：0:否;1:是;2:失敗',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'HasSend'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'告警信' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'TB_AlertMailLog'
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'建立時間',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'MKTime'
-GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'更新時間',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'TB_AlertMailLog',
-    @level2type = N'COLUMN',
-    @level2name = N'UPDTime'
+
