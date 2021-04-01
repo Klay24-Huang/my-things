349b6b5e74816acb20cc99ea7e90441d4612aecd
BensonTang <benson922@hotaimotor.com.tw>
2021-04-01 10:32:29 +0800

    改光陽維運報表撈取限制條件

--- Web/Scripts/js/ActionJS/Report/KymcoQuery.js
+++ Web/Scripts/js/ActionJS/Report/KymcoQuery.js
@@ -72,9 +72,9 @@
                 errMsg = "起始日期大於結束日期";
             } else {
                 var GetDateDiff = DateDiff(SD, ED);
-                if (GetDateDiff > 35) {
+                if (GetDateDiff > 3) {
                     flag = false;
-                    errMsg = "時間區間不可大於35天";
+                    errMsg = "時間區間不可大於3天";
                 }
             }
         } else {
