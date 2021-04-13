ba97ed21b70eba2deac044ccdf447fd654fd99d9
Jet_Yeh <HICEIT2@hotaimotor.com.tw>
2021-04-13 10:00:35 +0800

    取得Banner資訊：For安卓測試URL

--- WebAPI/Models/Enum/ObjType.cs
+++ WebAPI/Models/Enum/ObjType.cs
@@ -948,7 +948,8 @@ namespace WebAPI.Models.Enum
                     SPName = "usp_GetCarReturnMillage";
                     break;
                 case SPType.GetBanner:  // 取得廣告資訊 20210316 ADD
-                    SPName = "usp_GetBanner";
+                    //SPName = "usp_GetBanner";
+                    SPName = "usp_GetBanner_20210413";      //For安卓測試
                     break;
                 case SPType.GetCarTypeGroupList:   
                     SPName = "usp_GetCarTypeGroupList_Q1";
