75d8eda5c53f699d5a882beef3fb5a08132cb78c
gogolucky168 <gogolucky168@gmail.com>
2021-04-13 14:49:43 +0800

    vm建立(未完成)

---
+++ WebAPI/Models/Param/Bill/Input/IFN_TSIBCardTrade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Bill.Input
+{
+    public class IFN_TSIBCardTrade
+    {
+        public string IDNO { get; set; }
+
+        public string ProdNm { get; set; }
+        /// <summary>
+        /// EC交易序號
+        ///最大接受欄位長度為50碼，但實際長度依據商戶設定為準
+        /// </summary>
+        public string MerchantTradeNo { get; set; }
+    }
+}
