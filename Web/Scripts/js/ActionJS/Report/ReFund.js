c281b8ea1c0536a84f637114e182e38eca08067c
BensonTang <benson922@hotaimotor.com.tw>
2021-05-07 17:33:50 +0800

    退款功能強化

--- Web/Scripts/js/ActionJS/Report/ReFund.js
+++ Web/Scripts/js/ActionJS/Report/ReFund.js
@@ -20,6 +20,23 @@
 
     });
 
+    $("#0bt").on("click", function () {
+        //ShowLoading("資料查詢中…");
+        $("#orderNo").val($("#0oNo").text())
+        $("#NAME").val($("#0mNa").text())
+        $("#merchantOrderNo").val($("#0mNo").text())
+        $("#GIFTNAME").val($("#0pNa").text())
+
+    });
+    $("#1bt").on("click", function () {
+        //ShowLoading("資料查詢中…");
+        $("#orderNo").val($("#1oNo").text())
+        $("#NAME").val($("#1mNa").text())
+        $("#merchantOrderNo").val($("#1mNo").text())
+        $("#GIFTNAME").val($("#1pNa").text())
+
+    });
+
     $("#btnSend").on("click", function () {
         ShowLoading("資料處理中…");
         Account = $("#Account").val();
