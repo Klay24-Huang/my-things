93202059f370721d144412d981b4461083a21ece
BensonTang <benson922@hotaimotor.com.tw>
2021-05-24 13:56:51 +0800

    會員徽章，後台新增功能完成

--- Web/Controllers/MemberManageController.cs
+++ Web/Controllers/MemberManageController.cs
@@ -17,6 +17,9 @@ using Domain.SP.Output;
 using WebCommon;
 using WebAPI.Models.BaseFunc;
 using Web.Models.Enum;
+using System.IO;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace Web.Controllers
 {
@@ -808,32 +811,27 @@ namespace Web.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult MedalMileStone(string AuditMode, string IDNO, string ChoiceSelect)
+        public ActionResult MedalMileStone(string AuditMode, string IDNO, string ChoiceSelect, HttpPostedFileBase fileImport)
         {
-            string Mode = AuditMode;
-            ViewData["Mode"] = Mode;
-
             ViewData["IDNO"] = IDNO;
             ViewData["AuditMode"] = AuditMode;
 
-            string errorLine = "";
-            string errorMsg = "";
-            ViewData["errorLine"] = null;
-            ViewData["IsShowMessage"] = null;
+            //string errorLine = "";
+            //string errorMsg = "";
+            //ViewData["errorLine"] = ""; 
+            //ViewData["errorMsg"] = ""; 
 
             if (AuditMode == "0")
             {
                 List<BE_MileStone> lstData = new MemberRepository(connetStr).GetMileStone(IDNO);
                 return View(lstData);
             }
-            else
+            else if (AuditMode == "1")
             {
                 ////MemberRepository obj = new MemberRepository(connetStr).InsertMileStone(IDNO,ChoiceSelect); //會錯
                 //MemberRepository obj = new MemberRepository(connetStr);
                 //obj.InsertMileStone(IDNO, ChoiceSelect);
                 //return View();
-
-
 
                 bool flag = true;
                 List<ErrorInfo> lstError = new List<ErrorInfo>();
@@ -848,24 +846,115 @@ namespace Web.Controllers
                 SPOutput_Base SPOutput = new SPOutput_Base();
                 flag = new SQLHelper<SPInput_BE_IneInsMileStone, SPOutput_Base>(connetStr).ExecuteSPNonQuery("usp_BE_InsMileStone", data, ref SPOutput, ref lstError);
                 baseVerify.checkSQLResult(ref flag, SPOutput.Error, SPOutput.ErrorCode, ref lstError, ref errCode);
-                if (flag == false)
-                {
-                    errorLine = "錯誤";
-                    errorMsg = "錯誤";
-                }
 
                 if (flag)
                 {
                     ViewData["errorLine"] = "ok";
+                    //return Content("<script>alert('ok');</script>");
                 }
                 else
                 {
-                    ViewData["errorMsg"] = errorMsg;
-                    ViewData["errorLine"] = errorLine;
+                    ViewData["errorLine"] = "fall";
                 }
+
+                return View();
+            }
+            else
+            {
+                //string errorLine = "";
+                string errorMsg = "";
+                bool flag = true;
+                ViewData["errorLine2"] = null;
+
+                List<SPInput_BE_IneInsMileStone> lstData = new List<SPInput_BE_IneInsMileStone>();
+                List<ErrorInfo> lstError = new List<ErrorInfo>();
+                string errCode = "";
+                CommonFunc baseVerify = new CommonFunc();
+                if (fileImport != null)
+                {
+                    if (fileImport.ContentLength > 0)
+                    {
+                        string fileName = string.Concat(new string[]{
+                            "MedalMileStoneImport",
+                            ((Session["Account"]==null)?"":Session["Account"].ToString()),
+                            "_",
+                            DateTime.Now.ToString("yyyyMMddHHmmss"),
+                            ".xlsx"
+                        });
+                        DirectoryInfo di = new DirectoryInfo(Server.MapPath("~/Content/upload/MedalMileStoneImport"));
+                        if (!di.Exists)
+                        {
+                            di.Create();
+                        }
+                        string path = Path.Combine(Server.MapPath("~/Content/upload/MedalMileStoneImport"), fileName);
+                        fileImport.SaveAs(path);
+                        IWorkbook workBook = new XSSFWorkbook(path);
+                        ISheet sheet = workBook.GetSheetAt(0);
+                        int sheetLen = sheet.LastRowNum;
+                        string[] field = { "ID", "匯入項目" };
+                        int fieldLen = field.Length;
+                        //第一關，判斷位置是否相等
+                        for (int i = 0; i < fieldLen; i++)
+                        {
+                            ICell headCell = sheet.GetRow(0).GetCell(i);
+                            if (headCell.ToString().Replace(" ", "").ToUpper() != field[i])
+                            {
+                                errorMsg = "標題列不相符";
+                                flag = false;
+                                break;
+                            }
+                        }
+                        //通過第一關 
+                        if (flag)
+                        {
+                            string UserId = ((Session["Account"] == null) ? "" : Session["Account"].ToString());
+                            //string SPName = new ObjType().GetSPName(ObjType.SPType.InsTransParking);
+                            for (int i = 1; i <= sheetLen; i++)
+                            {
+                                SPInput_BE_IneInsMileStone data = new SPInput_BE_IneInsMileStone()
+                                {
+                                    IDNO = sheet.GetRow(i).GetCell(0).ToString().Replace(" ", ""),
+                                    ChoiceSelect = sheet.GetRow(i).GetCell(1).ToString().Replace(" ", ""),
+                                    USERID = UserId
+                                };
+
+                                SPOutput_Base SPOutput = new SPOutput_Base();
+                                flag = new SQLHelper<SPInput_BE_IneInsMileStone, SPOutput_Base>(connetStr).ExecuteSPNonQuery("usp_BE_InsMileStone", data, ref SPOutput, ref lstError);
+                                baseVerify.checkSQLResult(ref flag, SPOutput.Error, SPOutput.ErrorCode, ref lstError, ref errCode);
+                                if (flag == false)
+                                {
+                                    //errorLine = i.ToString();
+                                    errorMsg = string.Format("寫入第{0}筆資料時，發生錯誤：{1}", i.ToString(), baseVerify.GetErrorMsg(errCode));
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        flag = false;
+                        errorMsg = "請上傳要匯入的資料";
+                    }
+                }
+                else
+                {
+                    flag = false;
+                    errorMsg = "請上傳要匯入的資料";
+                }
+
+                if (flag)
+                {
+                    ViewData["errorLine2"] = "ok";
+                }
+                else
+                {
+                    //ViewData["errorMsg2"] = errorMsg;
+                    ViewData["errorLine2"] = errorMsg;
+                }
+
+                return View();
             };
 
-            return View();
+            
         }
 
         
--- Web/Scripts/js/ActionJS/MemberManage/MedalMileStone.js
+++ Web/Scripts/js/ActionJS/MemberManage/MedalMileStone.js
@@ -5,11 +5,26 @@
         switch (Mode) {
             case "0":
                 $("#Choice").hide();
+                $("#btnSubmit").show();
                 $("#btnSubmit").text("查詢");
+                $("#Import").hide();
+                $("#ID").show();
+                $("#btnSubmit2").hide();
                 break;
             case "1":
                 $("#Choice").show();
+                $("#ID").show();
+                $("#Import").hide();
+                $("#btnSubmit").show();
                 $("#btnSubmit").text("新增");
+                $("#btnSubmit2").hide();
+                break;
+            case "2":
+                $("#btnSubmit2").show();
+                $("#btnSubmit").hide();
+                $("#Import").show();
+                $("#ID").hide();
+                $("#Choice").hide();
                 break;
         }
     });
@@ -17,30 +32,106 @@
     $("#btnSubmit").on("click", function () {
         ShowLoading("資料查詢中…");
         var flag = true;
-        //console.log('aaaaaa')
+
         if ($("#IDNO").val() == "") {
             flag = false;
             errMsg = "ID沒填";
         }
 
         if (flag) {
-            disabledLoading();
             $("#frmMedalMileStone").submit();
-            //console.log('ssssss')
+            disabledLoading();
+
+            //要花太多時間測試，先跳過
+            //var Mode = $("#AuditMode").val();
+            //if (Mode == "1") {
+            //    var SendObj = new Object();
+            //    SendObj.IDNO = $("#IDNO").val();
+            //    SendObj.TYPE = $("#ChoiceSelect").val();
+            //    var json = JSON.stringify(SendObj);
+            //    $.ajax({
+            //        url: '@(Url.Action("MedalMileStone", "MemberManage"))',
+            //        type: "POST",
+            //        data: json,
+            //        cache: false,
+            //        contentType: 'application/json',
+            //        dataType: "json",
+            //        success: function (data) {
+            //            if (data.Result == "1") {
+            //                swal({
+            //                    title: 'SUCCESS',
+            //                    text: data.ErrorMessage,
+            //                    icon: 'success'
+            //                }).then(function (value) {
+            //                    history.back();
+            //                });
+            //            } else {
+
+            //                swal({
+            //                    title: 'Fail1',
+            //                    text: data.ErrorMessage,
+            //                    icon: 'error'
+            //                });
+            //            }
+            //        }
+            //    });
+            //}
+            //else {
+            //    $("#frmMedalMileStone").submit();
+            //    disabledLoading();
+            //}
+
+            //沒有效
+            //setTimeout回調函數，等2秒再做裡面內容
+            //setTimeout(function () {
+            //    $("#frmMedalMileStone").submit();
+            //    disabledLoading();
+            //}, 2000); 
+
+            //var Mode = $("#AuditMode").val();
+            //if (Mode == "1") {
+            //    if (errorLine == "ok") {
+            //        ShowSuccessMessage("新增成功");
+            //    } else if (errorLine == "fall") {
+            //        ShowFailMessage(errorLine);
+            //    }
+            //}
+
         } else {
             disabledLoadingAndShowAlert(errMsg);
         }
 
-        var Mode = $('#modeData').val();//$("#AuditMode").val();
-        if (Mode == "Add") {
-            if (errorLine == "ok") {
-                ShowSuccessMessage("新增成功");
-            } else {
-                if (errorMsg != "") {
-                    ShowFailMessage(errMsg);
-                }
-            }
-        }
     });
 
+    $("#exampleDownload").on("click", function () {
+        window.location.href = "../Content/example/MileStoneExample.xlsx";
+    });
+
+    $("#btnSubmit2").on("click", function () {
+        ShowLoading("資料匯入中");
+        $("#frmMedalMileStone").submit();
+        disabledLoading();
+    });
+    $("#fileImport").on("change", function () {
+        var file = this.files[0];
+        var fileName = file.name;
+        var ext = GetFileExtends(fileName);
+        var extName = "";
+        if (CheckStorageIsNull(ext)) {
+            extName = ext[0];
+            console.log(extName.toUpperCase())
+        }
+        if (extName.toUpperCase() != "XLSX" && extName.toUpperCase() != "XLS") {
+
+            swal({
+                title: 'Fail',
+                text: "僅允許匯入xlsx或xls格式",
+                icon: 'error'
+            }).then(function (value) {
+
+                $("#fileImport").val("");
+            });
+        }
+    })
+    
 });
