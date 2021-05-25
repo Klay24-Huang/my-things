e85c51ab699799b85877fff1df94b4f05e754310
Aus <frank0308@hotaimotor.com.tw>
2021-05-25 17:23:50 +0800

    新增合約修改(無金流)

--- Web/Scripts/js/CommonFunc.js
+++ Web/Scripts/js/CommonFunc.js
@@ -247,6 +247,7 @@ function DoAjaxAfterReload(obj,API,FailMessage) {
     console.log("json:" + json);
     var site = jsHost + API;
     //var site = "http://localhost:2061/api/BE_HandleUserMaintain" //202012唐測試用
+    //var site = "http://localhost:2061/api/" + API
     console.log("site:" + site);
     $.ajax({
         url: site,
@@ -475,6 +476,7 @@ function DoAjaxAfterCallBackWithOutMessage(obj, API, FailMessage, CallBack) {
     var json = JSON.stringify(obj);
     console.log(json);
     var site = jsHost + API;
+    //var site = "http://localhost:2061/api/" + API
     console.log("site:" + site);
     $.ajax({
         url: site,
