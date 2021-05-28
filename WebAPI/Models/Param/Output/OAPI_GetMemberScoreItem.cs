7c2b531a82061504eea46781af8f4f2509737ddb
Aus <frank0308@hotaimotor.com.tw>
2021-05-28 13:24:42 +0800

    會員攻略積分網站API建置(暫時用不到但先保留)

--- WebAPI/Models/Param/Output/OAPI_GetMemberScoreItem.cs
+++ WebAPI/Models/Param/Output/OAPI_GetMemberScoreItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.TB.BackEnd;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,54 +8,6 @@ namespace WebAPI.Models.Param.Output
 {
     public class OAPI_GetMemberScoreItem
     {
-        /// <summary>
-        /// 分類編號
-        /// </summary>
-        public string SCTYPENO { get; set; }
-
-        /// <summary>
-        /// 分類
-        /// </summary>
-        public string SCTYPE { get; set; }
-
-        /// <summary>
-        /// 主項編號
-        /// </summary>
-        public string SCITEMNO { get; set; }
-
-        /// <summary>
-        /// 主項名稱
-        /// </summary>
-        public string SCITEM { get; set; }
-
-        /// <summary>
-        /// 次項編號
-        /// </summary>
-        public string SCMITEMNO { get; set; }
-
-        /// <summary>
-        /// 次項名稱
-        /// </summary>
-        public string SCMITEM { get; set; }
-
-        /// <summary>
-        /// 細項編號
-        /// </summary>
-        public string SCDITEMNO { get; set; }
-
-        /// <summary>
-        /// 細項名稱
-        /// </summary>
-        public string SCDITEM { get; set; }
-
-        /// <summary>
-        /// 用戶畫面
-        /// </summary>
-        public string UIDESC { get; set; }
-
-        /// <summary>
-        /// 分數
-        /// </summary>
-        public int SCORE { get; set; }
+        public List<BE_GetMemberScoreItem> datalst { get; set; }
     }
 }
