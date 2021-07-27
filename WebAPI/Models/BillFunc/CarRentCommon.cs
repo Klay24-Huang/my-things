0746319b2351d9def2c1b06d846184d5631e4692
gogolucky168 <gogolucky168@gmail.com>
2021-07-27 11:24:06 +0800

    sp_GetEstimate-BugFix

--- WebAPI/Models/BillFunc/CarRentCommon.cs
+++ WebAPI/Models/BillFunc/CarRentCommon.cs
@@ -1431,7 +1431,7 @@ namespace WebAPI.Models.BillFunc
 
             string SPName = new ObjType().GetSPName(ObjType.SPType.GetEstimate);
 
-            object[] param = new object[3];
+            object[] param = new object[4];
             param[0] = PROJID;
             param[1] = CARTYPE;
             param[2] = LogID;
