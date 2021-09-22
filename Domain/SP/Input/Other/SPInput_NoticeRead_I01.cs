89d40c85a82c4e6de76fa9904209f6492fde53e6
adamliu6646 <Adam.Liu@mail.searching-service.com>
2021-09-22 15:41:10 +0800

    推播通知讀取API交付

---
+++ Domain/SP/Input/Other/SPInput_NoticeRead_I01.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SP.Input.Other
+{
+    public class SPInput_NoticeRead_I01 : SPInput_Base
+    {
+        public string IDNO { get; set; }
+    }
+}
