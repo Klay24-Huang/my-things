153ded6da6acb05d237dc0255fc6087e145e1dde
UmekoHu <HICEIT4@hotaimotor.com.tw>
2021-10-26 13:13:55 +0800

    BRD-741 台新OtherService DoCreditCardAuthV3 Create

---
+++ Domain/SP/Input/OtherService/Taishin/SPInput_UpdTradeForClose.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SP.Input.OtherService.Taishin
+{
+    public class SPInput_UpdTradeForClose: SPInput_UpdTrade
+    {
+        public int AutoClose { get; set; }
+        /// <summary>
+        /// 信用卡類別 0:和泰、1:台新
+        /// </summary>
+        public int CardType { get; set; }
+
+        public string ProName { get; set; }
+
+        public string UserID { get; set; }
+
+    }
+}
