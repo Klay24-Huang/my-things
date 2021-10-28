65b068603421840bd1179eb460b78f6157f4d26d
NTDOMAIN\HICEIT5 <HICEIT5@hotaimotor.com.tw>
2021-10-28 13:42:34 +0800

    預約(Booking) 新增預授權機制，整理程式碼

---
+++ WebAPI/Service/CommonService.cs
@@ -0,0 +1,116 @@
+﻿using Domain.SP.Input.Bill;
+using Domain.SP.Input.Rent;
+using Domain.SP.Output;
+using Domain.SP.Output.Bill;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using WebAPI.Models.BaseFunc;
+using WebAPI.Utils;
+using WebCommon;
+
+namespace WebAPI.Service
+{
+    /// <summary>
+    /// 共用Service
+    /// </summary>
+    public class CommonService
+    {
+        CommonFunc baseVerify = new CommonFunc();
+        private string connetStr = ConfigurationManager.ConnectionStrings["IRent"].ConnectionString;
+
+        /// <summary>
+        /// 取得安心服務每小時價格
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="IDNO">會員編號</param>
+        /// <param name="CarTypeCode">車型代碼</param>
+        /// <param name="LogID"></param>
+        /// <param name="errCode">錯誤代碼</param>
+        /// <returns></returns>
+        public (bool checkFlag, int InsurancePerHours) GetInsurancePrice(string IDNO, string CarTypeCode, long LogID, ref string errCode)
+        {
+            (bool checkFlag, int InsurancePerHours) result = (false, 0);
+            int insurancePerHours = 0;
+            string spName = "usp_GetInsurancePrice";
+            //20201103 ADD BY ADAM REASON.取得安心服務每小時價格
+            SPInput_GetInsurancePrice spGetInsurancePrice = new SPInput_GetInsurancePrice()
+            {
+                IDNO = IDNO,
+                CarType = CarTypeCode,
+                LogID = LogID
+            };
+            List<SPOutput_GetInsurancePrice> re = new List<SPOutput_GetInsurancePrice>();
+            SPOutput_Base spOutput_Base = new SPOutput_Base();
+            SQLHelper<SPInput_GetInsurancePrice, SPOutput_Base> sqlHelp = new SQLHelper<SPInput_GetInsurancePrice, SPOutput_Base>(connetStr);
+            DataSet ds = new DataSet();
+            List<ErrorInfo> lstError = new List<ErrorInfo>();
+            bool flag = sqlHelp.ExeuteSP(spName, spGetInsurancePrice, ref spOutput_Base, ref re, ref ds, ref lstError);
+            baseVerify.checkSQLResult(ref flag, spOutput_Base.Error, spOutput_Base.ErrorCode, ref lstError, ref errCode);
+            if (flag && re.Count > 0)
+            {
+                insurancePerHours = int.Parse(re[0].InsurancePerHours.ToString());
+            }
+            result.checkFlag = flag;
+            result.InsurancePerHours = insurancePerHours;
+            return result;
+        }
+
+        /// <summary>
+        /// 檢查信用卡是否綁卡
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="IDNO">會員編號</param>
+        /// <param name="errCode"></param>
+        /// <returns></returns>
+        public (bool hasFind, string cardToken) CheckBindCard(ref bool flag, string IDNO, ref string errCode)
+        {
+            (bool hasFind, string cardToken) result = (false, "");
+            //20201219 ADD BY JERRY 更新綁卡查詢邏輯，改由資料庫查詢
+            DataSet ds = Common.getBindingList(IDNO, ref flag, ref errCode, ref errCode);
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                result.hasFind = true;
+                result.cardToken = ds.Tables[0].Rows[0]["CardToken"].ToString();
+            }
+            else
+            {
+                flag = false;
+                errCode = "ERR730";
+            }
+            ds.Dispose();
+            return result;
+        }
+
+
+
+        public bool sp_InsOrderAuthAmount(SPInput_InsOrderAuthAmount spInput, ref string errCode)
+        {
+            bool flag = false;
+            var lstError = new List<ErrorInfo>();
+            SPOutput_Base spOutput = new SPOutput_Base();
+            string SPName = "usp_OrderAuthAmount_I01";
+            flag = new SQLHelper<SPInput_InsOrderAuthAmount, SPOutput_Base>(connetStr).ExecuteSPNonQuery(SPName, spInput, ref spOutput, ref lstError);
+            if (flag)
+            {
+                if (spOutput.Error == 1 || spOutput.ErrorCode != "0000")
+                {
+                    flag = false;
+                    errCode = spOutput.ErrorCode;
+                }
+            }
+            else
+            {
+                if (lstError.Count > 0)
+                {
+                    errCode = lstError[0].ErrorCode;
+                }
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ iRentDBV2/dbo/Stored Procedures/usp_OrderAuthAmount_I01.sql
@@ -0,0 +1,124 @@
+﻿/***********************************************************************************************
+* Serve    : sqyhi03az.database.windows.net
+* Database : IRENT_V2
+* 程式名稱 : usp_OrderAuthAmount_I01
+* 系    統 : IRENT
+* 程式功能 : 寫入預授權金額檔
+* 作    者 : AMBER
+* 撰寫日期 : 20211027
+Example :
+***********************************************************************************************/
+CREATE PROCEDURE [dbo].[usp_OrderAuthAmount_I01]
+    @MerchantTradNo         VARCHAR(50)           , --廠商訂單編號
+    @BankTradeNo            VARCHAR(50)           , --銀行交易序號
+    @IDNO                   VARCHAR(10)           , --操作的會員帳號
+	@CardType               INT                   , --信用卡類別(0:和泰 ; 1:台新)
+	@AuthType               INT                   , --授權類別 (16:預約; 17:訂金; 18:延長用車; 19:取車; 20:逾時; 21:欠費; 22:還車)
+    @final_price            INT                   , --授權金額
+	@PRGName                VARCHAR(50)           , --程式名稱
+	@OrderNo                INT                   , --訂單編號
+    @Status                 INT                   , --處理狀態
+	@Token                  VARCHAR(1024)         , --JWT TOKEN
+	@LogID                  BIGINT                , --執行的api log
+	@ErrorCode 				VARCHAR(6)		OUTPUT,	--回傳錯誤代碼
+	@ErrorMsg  				NVARCHAR(100)	OUTPUT,	--回傳錯誤訊息
+	@SQLExceptionCode		VARCHAR(10)		OUTPUT,	--回傳sqlException代碼
+	@SQLExceptionMsg		NVARCHAR(1000)	OUTPUT	--回傳sqlException訊息
+AS
+DECLARE @Error INT
+DECLARE @IsSystem TINYINT
+DECLARE @FunName VARCHAR(50)
+DECLARE @ErrorType TINYINT
+DECLARE @hasData INT
+DECLARE @NowTime DATETIME
+
+/*初始設定*/
+SET @Error=0;
+SET @ErrorCode='0000';
+SET @ErrorMsg='SUCCESS'; 
+SET @SQLExceptionCode='';
+SET @SQLExceptionMsg='';
+
+SET @FunName='usp_OrderAuthAmount_I01';
+SET @IsSystem=0;
+SET @ErrorType=0;
+SET @IsSystem=0;
+SET @hasData=0;
+
+SET @NowTime         =dbo.GET_TWDATE();
+SET @OrderNo         =ISNULL (@OrderNo,'0')
+SET @IDNO            =ISNULL (@IDNO,'');
+SET @MerchantTradNo  =ISNULL (@MerchantTradNo,'');
+SET @BankTradeNo     =ISNULL (@BankTradeNo,'');
+SET @Token           =ISNULL (@Token,'');
+
+BEGIN TRY
+
+  IF @IDNO=''
+  BEGIN
+   SET @Error=1
+   SET @ErrorCode='ERR900'
+  END
+  		 
+  --0.再次檢核token
+  IF @Error=0
+  BEGIN
+    SELECT @hasData=COUNT(1) FROM TB_Token WITH(NOLOCK) WHERE  Access_Token=@Token  AND Rxpires_in>@NowTime;
+    IF @hasData=0
+    BEGIN
+      SET @Error=1;
+      SET @ErrorCode='ERR101';
+    END
+    ELSE
+	BEGIN
+	  SET @hasData=0;
+	  SELECT @hasData=COUNT(1) FROM TB_Token WITH(NOLOCK) WHERE  Access_Token=@Token AND MEMIDNO=@IDNO;
+	END 
+    IF @hasData=0
+	BEGIN
+		SET @Error=1;
+		SET @ErrorCode='ERR101';
+	END
+  END
+
+  IF @Error=0
+  BEGIN
+   DECLARE @PRGID VARCHAR(10) = '0'
+   SELECT @PRGID = Convert(VARCHAR(10),APIID) FROM TB_APIList WITH(NOLOCK) WHERE  APIName = @PRGName
+
+   IF NOT EXISTS (SELECT 1 FROM TB_OrderAuthAmount WHERE order_number=@OrderNo AND MerchantTradeNo=@MerchantTradNo)
+   BEGIN
+   INSERT INTO TB_OrderAuthAmount(order_number,MerchantTradeNo,BankTradeNo,IDNO,CardType,AuthType,final_price,[Status],A_PRGID,A_USERID,A_SYSDT,U_PRGID,U_USERID,U_SYSDT)	
+   VALUES(@OrderNo,@MerchantTradNo,@BankTradeNo,@IDNO,@CardType,@AuthType,@final_price,@Status,@PRGID,@PRGID,@NowTime,@PRGID,@PRGID,@NowTime);
+   END       
+  END
+
+--寫入錯誤訊息
+IF @Error=1
+BEGIN
+	INSERT INTO TB_ErrorLog(FunName,ErrorCode,ErrType,SQLErrorCode,SQLErrorDesc,LogID,IsSystem)
+		VALUES (@FunName,@ErrorCode,@ErrorType,@SQLExceptionCode,@SQLExceptionMsg,@LogID,@IsSystem);
+END
+
+END TRY
+BEGIN CATCH
+	SET @Error=-1;
+	SET @ErrorCode='ERR999';
+	SET @ErrorMsg='我要寫錯誤訊息';
+	SET @SQLExceptionCode=ERROR_NUMBER();
+	SET @SQLExceptionMsg=ERROR_MESSAGE();
+	IF @@TRANCOUNT > 0
+	BEGIN
+		print 'rolling back transaction' /* <- this is never printed */
+		ROLLBACK TRAN
+	END
+		SET @IsSystem=1;
+		SET @ErrorType=4;
+			INSERT INTO TB_ErrorLog(FunName,ErrorCode,ErrType,SQLErrorCode,SQLErrorDesc,LogID,IsSystem)
+			VALUES (@FunName,@ErrorCode,@ErrorType,@SQLExceptionCode,@SQLExceptionMsg,@LogID,@IsSystem);
+END CATCH
+RETURN @Error
+EXECUTE sp_addextendedproperty @name = N'Platform', @value = N'API', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'PROCEDURE', @level1name = N'usp_OrderAuthAmount_I01';
+
+
+
