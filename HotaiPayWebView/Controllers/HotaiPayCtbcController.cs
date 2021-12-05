0061384ceea77afbb20f9935e65ff8ba798c3e43
BensonTang <benson922@hotaimotor.com.tw>
2021-12-05 13:25:47 +0800

    和泰pay-webview刷卡2

--- HotaiPayWebView/Controllers/HotaiPayCtbcController.cs
+++ HotaiPayWebView/Controllers/HotaiPayCtbcController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Reposotory.Implement;
+using System.Configuration;
+using HotaiPayWebView.Models;
+using HotaiPayWebView.Repository;
 
 namespace HotaiPayWebView.Controllers
 {
     public class HotaiPayCtbcController : Controller
     {
+        private string connetStr = ConfigurationManager.ConnectionStrings["IRentT"].ConnectionString;
+        private static CommonRepository commonRepository = new CommonRepository(ConfigurationManager.ConnectionStrings["IRent"].ConnectionString);
+
         // GET: HotaiPayCtbc
         public ActionResult Index()
         {
             return View();
         }
+        //[HttpPost]
+        public ActionResult CreditCardChoose()
+        {
+            List<CreditCardChoose> lstData = new HotaiPayCtbcRepository(connetStr).GetCreditCarLists("C221120413");
+            return View(lstData);
+        }
+
     }
 }
