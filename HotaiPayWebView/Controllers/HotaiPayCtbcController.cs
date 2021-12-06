97df2dad4becae1f1d9faedaf191fd8c5be75bc5
BensonTang <benson922@hotaimotor.com.tw>
2021-12-06 13:24:30 +0800

    和泰pay-webview刷卡4

--- HotaiPayWebView/Controllers/HotaiPayCtbcController.cs
+++ HotaiPayWebView/Controllers/HotaiPayCtbcController.cs
@@ -7,6 +7,9 @@ using Reposotory.Implement;
 using System.Configuration;
 using HotaiPayWebView.Models;
 using HotaiPayWebView.Repository;
+using OtherService;
+using Domain.Flow.Hotai;
+using Domain.TB.Hotai;
 
 namespace HotaiPayWebView.Controllers
 {
@@ -23,12 +26,42 @@ namespace HotaiPayWebView.Controllers
         //[HttpPost]
         public ActionResult CreditCardChoose()
         {
-            List<CreditCardChoose> lstData = new HotaiPayCtbcRepository(connetStr).GetCreditCarLists("C221120413");
-            return View(lstData);
+            //List<CreditCardChoose> lstData = new HotaiPayCtbcRepository(connetStr).GetCreditCarLists("C221120413");
+            bool flag;
+            HotaipayService getlist = new HotaipayService();
+            IFN_QueryCardList input = new IFN_QueryCardList();
+            OFN_HotaiCreditCardList output = new OFN_HotaiCreditCardList();
+            IFN_QueryDefaultCard input2 = new IFN_QueryDefaultCard();
+            HotaiCardInfo card = new HotaiCardInfo();
+            string errorcode="";
+            input.IDNO = "A225668592";
+            input.PRGName = "CreditCardChoose";
+            flag = getlist.DoQueryCardList(input, ref output, ref errorcode);
+            //if (flag)
+            //{
+            //    flag = getlist.DoQueryDefaultCard(input2, ref card, ref errorcode);
+            //}
+
+            //CreditCardChoose Data = new CreditCardChoose();
+            List<HotaiCardInfo> Data = output.CreditCards;
+            //Data.CardNo = "";
+            //Data.CardType = "";
+
+            return View(Data);
         }
 
         public ActionResult ChooseNewCard()
         {
+            return View();
+        }
+        public ActionResult InsPersonInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsPersonInfo(string ID,string BIRTHDATE)
+        {
+            string b = ID;
             return View();
         }
     }
