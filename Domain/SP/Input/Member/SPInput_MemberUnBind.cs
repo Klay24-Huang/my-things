c8c92569cf8513a683d4d286396b067744ef757e
JetYeh <hiceit2@hotaimotor.com.tw>
2021-12-07 17:14:21 +0800

    會員解綁(MemberUnbind)：解綁邏輯調整，將會員相關檔案搬走，並將資料清回預設值

---
+++ Domain/SP/Input/Member/SPInput_MemberUnBind.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.SP.Input.Member
+{
+    public class SPInput_MemberUnBind : SPInput_Base
+    {
+        /// <summary>
+        /// 帳號
+        /// </summary>
+        public string IDNO { get; set; }
+
+        /// <summary>
+        /// Token
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// 功能名稱
+        /// </summary>
+        public string APIName { get; set; }
+    }
+}
