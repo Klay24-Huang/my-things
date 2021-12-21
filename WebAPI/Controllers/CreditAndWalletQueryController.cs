88de6ee492fc733b7579c02d8e4ece33980e7a32
YankeyLin <chi88137@hotmail.com>
2021-12-21 13:10:08 +0800

    查詢錢包餘額改抓台新紀錄餘額

--- WebAPI/Controllers/CreditAndWalletQueryController.cs
+++ WebAPI/Controllers/CreditAndWalletQueryController.cs
@@ -22,14 +22,17 @@ using WebCommon;
 using WebAPI.Utils;
 using Domain.Flow.Hotai;
 using Domain.TB.Hotai;
+using NLog;
 
 namespace WebAPI.Controllers
 {
+
     /// <summary>
     /// 查詢綁卡及錢包
     /// </summary>
     public class CreditAndWalletQueryController : ApiController
     {
+        protected static Logger logger = LogManager.GetCurrentClassLogger();
         private string connetStr = ConfigurationManager.ConnectionStrings["IRent"].ConnectionString;
         private string TaishinAPPOS = ConfigurationManager.AppSettings["TaishinAPPOS"].ToString();
         private string ApiVer = ConfigurationManager.AppSettings["ApiVerOther"].ToString();
@@ -49,6 +52,7 @@ namespace WebAPI.Controllers
             string errMsg = "Success"; //預設成功
             string errCode = "000000"; //預設成功
             string funName = "CreditAndWalletQueryController";
+            int TSB_WalletBalance = 0;
             Int64 LogID = 0;
             Int16 ErrType = 0;
             IAPI_CreditAndWalletQuery apiInput = null;
@@ -175,6 +179,7 @@ namespace WebAPI.Controllers
                     {
                         apiOutput.HasWallet = 1;
                     }
+                    TSB_WalletBalance = statusOutput.Result.Amount;
                 }
             }
             #endregion
@@ -226,12 +231,14 @@ namespace WebAPI.Controllers
                 {
                     apiOutput.PayMode = spOut.PayMode;
                     apiOutput.HasWallet = spOut.WalletStatus == "2" ? 1 : 0;
-                    apiOutput.TotalAmount = spOut.WalletAmout;
+                    //apiOutput.TotalAmount = spOut.WalletAmout; //20211221 UPD BY YANKEY REASON:改抓台新紀錄的餘額
+                    apiOutput.TotalAmount = TSB_WalletBalance;
                     apiOutput.MEMSENDCD = spOut.MEMSENDCD;
                     apiOutput.UNIMNO = spOut.UNIMNO;
                     apiOutput.CARRIERID = spOut.CARRIERID;
                     apiOutput.NPOBAN = spOut.NPOBAN;
                     apiOutput.AutoStored = spOut.AutoStored;
+                    logger.Trace($"\n\tIDNO = {IDNO} \n\t HasWallet = {apiOutput.HasWallet} \n\t TSB_WalletBalance = {TSB_WalletBalance}\n\t spOut.WalletAmout ={spOut.WalletAmout}");
                 }
             }
             #endregion
