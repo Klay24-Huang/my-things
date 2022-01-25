ab6ea4e793fe0f361afa7a9393bf3b512c947933
JetYeh <hiceit2@hotaimotor.com.tw>
2022-01-25 17:08:50 +0800

    GetMonthList：調整城市車手判斷邏輯

--- WebAPI/Models/Param/Output/OAPI_GetMonthList.cs
+++ WebAPI/Models/Param/Output/OAPI_GetMonthList.cs
@@ -1,33 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using WebAPI.Models.Param.Output.PartOfParam;
 
 namespace WebAPI.Models.Param.Output
 {
     public class OAPI_GetMonthList
     {
+        /// <summary>
+        /// 模式(1:月租，2我的所有方案)
+        /// </summary>
         public int ReMode { get; set; } = 0;
     }
 
     public class OAPI_AllMonthList_Car: OAPI_GetMonthList
     {
+        /// <summary>
+        /// 是否為機車
+        /// </summary>
         public int IsMotor { get; set; }
+        /// <summary>
+        /// 汽車牌卡
+        /// </summary>
         public List<MonCardParam> NorMonCards { get; set; }
-
+        /// <summary>
+        /// 城市車手牌卡
+        /// </summary>
         public List<MonCardParam> MixMonCards { get; set; }
     }
 
     public class OAPI_AllMonthList_Moto : OAPI_GetMonthList
     {
+        /// <summary>
+        /// 是否為機車
+        /// </summary>
         public int IsMotor { get; set; }
+        /// <summary>
+        /// 機車牌卡
+        /// </summary>
         public List<MonCardParam> NorMonCards { get; set; }
     }
 
     public class OAPI_MyMonthList: OAPI_GetMonthList
     {
+        /// <summary>
+        /// 汽車牌卡
+        /// </summary>
         public MonCardParam_My MyCar { get; set; }
+        /// <summary>
+        /// 機車牌卡
+        /// </summary>
         public MonCardParam_My MyMoto { get; set; }
     } 
 }
