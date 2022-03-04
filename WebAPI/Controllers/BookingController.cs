db108e56fa99391430d8bc54856448ff27f3f50f
NTDOMAIN\HICEIT5 <HICEIT5@hotaimotor.com.tw>
2022-03-04 16:45:05 +0800

    預約(Booking):預設支付方式補TraceLog、MD檔更新

--- WebAPI/Controllers/BookingController.cs
+++ WebAPI/Controllers/BookingController.cs
@@ -402,6 +402,8 @@ namespace WebAPI.Controllers
                 };
                 var wallet = walletSp.sp_GetPayInfo(getPayInfo, ref errCode);
                 defaultPayMode = wallet.PayMode[0].DefPayMode;
+                trace.traceAdd("sp_GetPayInfo",new { getPayInfo, defaultPayMode } );
+                trace.FlowList.Add("預設支付方式");
                 #endregion
                 #region 計算預扣款金額
                 if (ProjType == 0 || ProjType == 3)
