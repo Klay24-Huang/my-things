69e85269ce2048574fa09da6793ed36348051e88
adamliu6646 <Adam.Liu@mail.searching-service.com>
2022-03-14 10:56:06 +0800

    設定值調整hotaipay，增加錢包工具

---
+++ WebAPI/Models/Param/Input/IAPI_WalletTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Input
+{
+    public class IAPI_WalletTools
+    {
+        /// <summary>
+        /// 會員虛擬帳號
+        /// </summary>
+        public string AccountID { get; set; }
+        /// <summary>
+        /// 功能名稱 目前支援以下項目
+        /// AccountStatus
+        /// AccountValue
+        /// PayTransaction 儲值退款
+        /// </summary>
+        public string FuncName { get; set; }
+        /// <summary>
+        /// 交易金額 
+        /// </summary>
+        public int PayAmount { get; set; } = 0;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string USERID { get; set; }
+    }
+}
