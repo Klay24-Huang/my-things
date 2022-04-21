5110cdd52cac14ca920da87bfd10d2eed39b52cb
JetYeh <hiceit2@hotaimotor.com.tw>
2022-04-21 11:37:04 +0800

    配合Android開發，物件不可以是null，加入預設值。

--- WebAPI/Models/Param/Output/OAPI_AnyRent.cs
+++ WebAPI/Models/Param/Output/OAPI_AnyRent.cs
@@ -1,8 +1,6 @@
 ﻿using Domain.TB;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace WebAPI.Models.Param.Output
 {
@@ -25,38 +23,45 @@ namespace WebAPI.Models.Param.Output
         /// 訂閱制月租名稱
         /// </summary>
         public string MonProjNM { get; set; } = "";
+
         /// <summary>
         /// 汽車平日時數
         /// </summary>
         public double CarWDHours { get; set; } = 0;
+
         /// <summary>
         /// 汽車假日時數
         /// </summary>
         public double CarHDHours { get; set; } = 0;
+
         /// <summary>
         /// 機車不分平假日時數
         /// </summary>
         public int MotoTotalMins { get; set; } = 0;
+
         /// <summary>
         /// 汽車平日優惠價
         /// </summary>
         public double WDRateForCar { get; set; } = 0;
+
         /// <summary>
         /// 汽車假日優惠價
         /// </summary>
         public double HDRateForCar { get; set; } = 0;
+
         /// <summary>
         /// 機車平日優惠價
         /// </summary>
         public double WDRateForMoto { get; set; } = 0;
+
         /// <summary>
         /// 機車假日優惠價
         /// </summary>
         public double HDRateForMoto { get; set; } = 0;
+
         /// <summary>
         /// 優惠標籤
         /// </summary>
-        public DiscountLabel DiscountLabel { get; set; }
+        public DiscountLabel DiscountLabel { get; set; } = new DiscountLabel();
     }
-
 }
