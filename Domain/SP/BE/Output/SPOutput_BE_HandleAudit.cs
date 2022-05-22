cb16934b87bd5a269fe969eb0c14cf3e2afe0f26
adamliu6646 <Adam.Liu@mail.searching-service.com>
2022-05-22 21:05:21 +0800

    增加首次審核判斷

---
+++ Domain/SP/BE/Output/SPOutput_BE_HandleAudit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.SP.Output;
+
+namespace Domain.SP.BE.Output
+{
+    public class SPOutput_BE_HandleAudit : SPOutput_Base
+    {
+        /// <summary>
+        /// 首次審核 20220521 ADD BY ADAM 
+        /// </summary>
+        public string FirstAudit { get; set; }
+    }
+}
