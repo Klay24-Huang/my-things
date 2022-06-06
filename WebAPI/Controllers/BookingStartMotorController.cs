f585a6e3756f3ebf2c5e6fce549faef3bd6a3cd7
NTDOMAIN\HICEIT3 <HICEIT3@hotaimotor.com.tw>
2022-06-06 14:34:00 +0800

    Revert "接到Insurance參數，判斷存取"
    This reverts commit f8be972dc6b8f9642ddd8bd5fd71096b495a555e.

--- WebAPI/Controllers/BookingStartMotorController.cs
+++ WebAPI/Controllers/BookingStartMotorController.cs
@@ -259,7 +259,7 @@ namespace WebAPI.Controllers
                     if (flag)
                     {
                         //20211012 ADD BY ADAM REASON.增加手機定位點
-                        string BookingStartName = "usp_BookingStart_U01";
+                        string BookingStartName = "usp_BookingStart";
                         Domain.SP.Input.Rent.SPInput_BookingStart SPBookingStartInput = new Domain.SP.Input.Rent.SPInput_BookingStart()
                         {
                             IDNO = IDNO,
@@ -268,7 +268,7 @@ namespace WebAPI.Controllers
                             Token = Access_Token,
                             NowMileage = Convert.ToSingle(mil),
                             StopTime = "",
-                            Insurance = apiInput.Insurance //20220526 ADD BY 映韓 REASON.增加Insurance apiInput
+                            Insurance = 0
                             //20211012 ADD BY ADAM REASON.增加手機定位點
                             //PhoneLat = apiInput.PhoneLat,
                             //PhoneLon = apiInput.PhoneLon
