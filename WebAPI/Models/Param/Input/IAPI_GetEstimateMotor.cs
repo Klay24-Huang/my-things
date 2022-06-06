dfd9dfd8806d19d308703ca1322114e46c88bf40
JetYeh <hiceit2@hotaimotor.com.tw>
2022-06-06 14:19:19 +0800

    1.新增機車預估金額(未完成) 2.新增計算機車安心服務

---
+++ WebAPI/Models/Param/Input/IAPI_GetEstimateMotor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Input
+{
+    public class IAPI_GetEstimateMotor
+    {
+        /// <summary>
+        /// 專案代碼
+        /// </summary>
+        public string ProjID { get; set; }
+
+        /// <summary>
+        /// 車號
+        /// </summary>
+        public string CarNo { get; set; }
+
+        /// <summary>
+        /// 車型
+        /// </summary>
+        public string CarType { get; set; }
+
+        /// <summary>
+        /// 預計取車時間
+        /// </summary>
+        public string SDate { get; set; }
+
+        /// <summary>
+        /// 預計還車時間
+        /// </summary>
+        public string EDate { get; set; }
+
+        /// <summary>
+        /// 是否加購安心服務
+        /// <para>0:否</para>
+        /// <para>1:是</para>
+        /// </summary>
+        public int Insurance { get; set; }
+
+        /// <summary>
+        /// 訂閱制ID
+        /// </summary>
+        public int MonId { get; set; } = 0;
+    }
+}
---
+++ WebAPI/Models/Param/Output/OAPI_GetEstimateMotor.cs
@@ -0,0 +1,47 @@
+﻿using Domain.TB;
+
+namespace WebAPI.Models.Param.Output
+{
+    public class OAPI_GetEstimateMotor
+    {
+        /// <summary>
+        /// 租金
+        /// </summary>
+        public int CarRentBill { get; set; }
+
+        /// <summary>
+        /// 安心服務基本分鐘
+        /// </summary>
+        public int InsuranceBaseMinute { get; set; }
+
+        /// <summary>
+        /// 安心服務基本費用
+        /// </summary>
+        public double InsuranceBaseRate { get; set; }
+
+        /// <summary>
+        /// 機車安心服務分鐘(幾分鐘算一次錢)
+        /// </summary>
+        public double InsuranceMotoMin { get; set; }
+
+        /// <summary>
+        /// 機車安心服務金額(計算1次多少錢)
+        /// </summary>
+        public double InsuranceMotoRate { get; set; }
+
+        /// <summary>
+        /// 安心服務費用
+        /// </summary>
+        public int InsuranceBill { get; set; }
+
+        /// <summary>
+        /// 總計
+        /// </summary>
+        public int TotalBill { get; set; }
+
+        /// <summary>
+        /// 優惠標籤
+        /// </summary>
+        public EstimateDiscountLabel DiscountLabel { get; set; } = new EstimateDiscountLabel();
+    }
+}
