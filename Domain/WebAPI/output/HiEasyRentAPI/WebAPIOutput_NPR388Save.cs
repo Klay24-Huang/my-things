35c30c0219e6b5b81f566edafeaf794b7e7d7259
Aus <frank0308@hotaimotor.com.tw>
2022-06-29 15:41:53 +0800

    新增強換贈點API

---
+++ Domain/WebAPI/output/HiEasyRentAPI/WebAPIOutput_NPR388Save.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.WebAPI.output.HiEasyRentAPI
+{
+    public class WebAPIOutput_NPR388Save
+    {
+        /// <summary>
+        /// 處理結果
+        /// <para>True:成功</para>
+        /// <para>False:失敗</para>
+        /// </summary>
+        public bool Result { set; get; }
+        public string RtnCode { set; get; }
+        /// <summary>
+        /// 回覆訊息
+        /// </summary>
+        public string Message { set; get; }
+    }
+}
---
+++ WebAPI/Models/Param/Input/IAPI_BE_GiveGiftMins.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Input
+{
+    public class IAPI_BE_GiveGiftMins
+    {
+        /// <summary>
+        /// 會員流水號
+        /// </summary>
+        public string MEMRFNBR { get; set; }
+        /// <summary>
+        /// 贈與分鐘數
+        /// </summary>
+        public int GIFTMINS { get; set; }
+        /// <summary>
+        /// 優惠名稱
+        /// </summary>
+        public string COUPONNAME { get; set; }
+        /// <summary>
+        /// 贈點類型(01:汽車, 02:機車)
+        /// </summary>
+        public string GIFTTYPE { get; set; }
+        /// <summary>
+        /// 起日
+        /// </summary>
+        public string SDATE { get; set; }
+        /// <summary>
+        /// 迄日
+        /// </summary>
+        public string EDATE { get; set; }
+    }
+}
