1ac014f3a05c97a43a2d6e2e01e5774244d1a8f3
adamliu66466 <adamliu6646@hotaimotor.com.tw>
2022-06-30 19:01:08 +0800

    ftp改啟用ssl機制，並調整ftp處理邏輯

---
+++ WebAPI/Models/Param/Input/IAPI_FtpCityPark.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Input
+{
+    public class IAPI_FtpCityPark
+    {
+        /// <summary>
+        /// 處理日期 YYYYMMDD
+        /// </summary>
+        public string process_date { get; set; }
+    }
+}
