abbe988272b2331e8e329b924039791177eacba6
eddieyensc <eddieyensc@hotaimotor.com.tw>
2022-07-04 16:12:54 +0800

    完成

---
+++ Domain/SP/Input/Enterprise/SPInput_DeleteEnterpriseUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SP.Input.Enterprise
+{
+    public class SPInput_DeleteEnterpriseUser
+    {
+        public string Token { set; get; }
+
+        public Int64 LogID { set; get; }
+
+        /// <summary>
+        /// APIName
+        /// </summary>
+        public string ApiName { get; set; }
+    }
+}
---
+++ WebAPI/Models/Param/Input/IAPI_SetEnterpriseUserMode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models.Param.Input
+{
+    public class IAPI_SetEnterpriseUserMode
+    {
+        /// <summary>
+        /// 統一編號
+        /// </summary>
+        public string TaxID { get; set; }
+
+        /// <summary>
+        /// 公司名稱
+        /// </summary>
+        public string CompanyName { get; set; }
+
+        /// <summary>
+        /// 單位部門
+        /// </summary>
+        public int Depart { get; set; }
+
+        /// <summary>
+        /// 員工編號
+        /// </summary>
+        public string EmployeeID { get; set; }
+
+        /// <summary>
+        /// 員工姓名
+        /// </summary>
+        public string MEMCNAME { get; set; }
+    }
+}
