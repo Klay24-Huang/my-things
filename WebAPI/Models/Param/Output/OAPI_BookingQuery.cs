8ee750aaa8a6b089a8ede537a5a9a02c29be0bcd
adamliu66466 <adamliu6646@hotaimotor.com.tw>
2022-07-14 10:44:46 +0800

    拿掉原本Data裡面的 NowOrderFlg

--- WebAPI/Models/Param/Output/OAPI_BookingQuery.cs
+++ WebAPI/Models/Param/Output/OAPI_BookingQuery.cs
@@ -11,8 +11,9 @@ namespace WebAPI.Models.Param.Output
         /// <summary>
         /// 20220707 ADD BY ADAM REASON.增加是否有有效訂單判斷
         /// 目前是否有效訂單 Y/N
+        /// 20220714 ADD BY ADAM REASON.Levy哥說要拿掉
         /// </summary>
-        public string NowOrderFlg { set; get; } = "N";
+        //public string NowOrderFlg { set; get; } = "N";
         public List<ActiveOrderData> OrderObj { set; get; }
     }
 }
