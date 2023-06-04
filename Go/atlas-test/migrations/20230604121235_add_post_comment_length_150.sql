0859a38c13f60027e32975130547ead87cdf655b
Hao-Xiang Huang <sean200365@hotmail.com>
2023-06-05 01:12:47 +0800

    add new example

---
+++ Go/atlas-test/migrations/20230604121235_add_post_comment_length_150.sql
@@ -0,0 +1,2 @@
+-- Modify "blog_posts" table
+ALTER TABLE `blog_posts` MODIFY COLUMN `comment` varchar(150) NULL;
---
+++ Go/atlas-test/migrations/20230604122653_add_user_new_name_length_200.sql
@@ -0,0 +1,2 @@
+-- Modify "users" table
+ALTER TABLE `users` MODIFY COLUMN `new_name` varchar(250) NULL;
