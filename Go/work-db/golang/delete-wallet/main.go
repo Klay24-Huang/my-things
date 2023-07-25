9a891cb41ff01e96765369e41df96a4cb3256d5f
klay <klay_huang@taihe.asia>
2023-07-25 10:36:57 +0800

    add mongo db code

--- Go/work-db/golang/delete-wallet/main.go
+++ Go/work-db/golang/delete-wallet/main.go
@@ -8,12 +8,35 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/zngw/sshtunnel/ssht"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	if err := godotenv.Load("config.env"); err != nil {
+		log.Println("No .env file found")
+	}
+
+	foo := os.Getenv("FOO")
+	fmt.Println(foo)
+
+	sshUri := "ssh://"
+	mongoUri := ""
+
+	listen, err := ssht.TunnelUri(sshUri, u.Host, ":3717")
+	if err != nil {
+		log.Fatal("ssh failed %v", err)
+	} else {
+		log.Println("ssh successed %s", listen)
+	}
+}
+
+// 連ssh
+// https://www.jianshu.com/p/e3617a2883ea
+
+func foo() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
