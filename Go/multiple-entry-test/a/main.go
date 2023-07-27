a4a716cb3db55db04a19be75816930d5b110af5f
klay <klay_huang@taihe.asia>
2023-07-27 17:41:30 +0800

    add viper example

--- Go/multiple-entry-test/a/main.go
+++ Go/multiple-entry-test/a/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
+	viper.SetConfigName("a")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	test := viper.GetString("foo.bar")
+
+	log.Println(test)
+
 	fmt.Println("this is a")
 }
