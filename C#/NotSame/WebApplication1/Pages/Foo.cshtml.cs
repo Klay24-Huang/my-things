0dd5f9009ac708dc1f8422bfd1a2f49132b4ee53
Klay <wkus963@gmail.com>
2023-09-22 14:17:58 +0800

    add test project

---
+++ C#/NotSame/WebApplication1/Pages/Foo.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class FooModel : PageModel
+    {
+        private readonly ILogger<FooModel> _logger;
+
+        public FooModel(ILogger<FooModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
