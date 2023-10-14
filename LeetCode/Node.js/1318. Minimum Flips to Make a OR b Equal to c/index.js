f8b96b40c442f02aec58f36e140f5a402c963573
Klay <wkus963@gmail.com>
2023-10-14 22:41:55 +0800

    change prettier formating

--- LeetCode/Node.js/1318. Minimum Flips to Make a OR b Equal to c/index.js
+++ LeetCode/Node.js/1318. Minimum Flips to Make a OR b Equal to c/index.js
@@ -5,23 +5,23 @@
  * @return {number}
  */
 var minFlips = function (a, b, c) {
-    let count = 0
-    // console.log('max', Math.max(a, b, c))
-    for (let i = 0; 1 << i <= Math.max(a, b, c); i++) {
-        let countOne = 0
-        let currBit = 1 << i
-        if (currBit & a) countOne++
-        if (currBit & b) countOne++
-        if (currBit & c) {
-            // is one
-            if (countOne === 0) count++
-        } else {
-            // is zero
-            count += countOne
-        }
-        // console.log('turn', i, count)
-    }
-    return count
+	let count = 0;
+	// console.log('max', Math.max(a, b, c))
+	for (let i = 0; 1 << i <= Math.max(a, b, c); i++) {
+		let countOne = 0;
+		let currBit = 1 << i;
+		if (currBit & a) countOne++;
+		if (currBit & b) countOne++;
+		if (currBit & c) {
+			// is one
+			if (countOne === 0) count++;
+		} else {
+			// is zero
+			count += countOne;
+		}
+		// console.log('turn', i, count)
+	}
+	return count;
 };
 
-export default minFlips
+export default minFlips;
--- LeetCode/Node.js/2300. Successful Pairs of Spells and Potions/index.test.js
+++ LeetCode/Node.js/2300. Successful Pairs of Spells and Potions/index.test.js
@@ -1,54 +1,58 @@
-import successfulPairs from '.'
+import successfulPairs from ".";
 
-it('test', () => {
-    const tests = [{
-            title: "case 1",
-            input: {
-                spells: [5, 1, 3],
-                potions: [1, 2, 3, 4, 5],
-                success: 7,
-            },
-            expected: [4, 0, 3]
-        },
-        {
-            title: "case 2",
-            input: {
-                spells: [3, 1, 2],
-                potions: [8, 5, 8],
-                success: 16,
-            },
-            expected: [2, 0, 2]
-        },
-        {
-            title: "case 3",
-            input: {
-                spells: [15, 8, 19],
-                potions: [38, 36, 23],
-                success: 328,
-            },
-            expected: [3, 0, 3]
-        },
-        {
-            title: "case 4",
-            input: {
-                spells: [39, 34, 6, 35, 18, 24, 40],
-                potions: [27, 37, 33, 34, 14, 7, 23, 12, 22, 37],
-                success: 43,
-            },
-            expected: [10, 10, 9, 10, 10, 10, 10]
-        },
-    ]
+it("test", () => {
+	const tests = [
+		{
+			title: "case 1",
+			input: {
+				spells: [5, 1, 3],
+				potions: [1, 2, 3, 4, 5],
+				success: 7,
+			},
+			expected: [4, 0, 3],
+		},
+		{
+			title: "case 2",
+			input: {
+				spells: [3, 1, 2],
+				potions: [8, 5, 8],
+				success: 16,
+			},
+			expected: [2, 0, 2],
+		},
+		{
+			title: "case 3",
+			input: {
+				spells: [15, 8, 19],
+				potions: [38, 36, 23],
+				success: 328,
+			},
+			expected: [3, 0, 3],
+		},
+		{
+			title: "case 4",
+			input: {
+				spells: [39, 34, 6, 35, 18, 24, 40],
+				potions: [27, 37, 33, 34, 14, 7, 23, 12, 22, 37],
+				success: 43,
+			},
+			expected: [10, 10, 9, 10, 10, 10, 10],
+		},
+	];
 
-    for (const test of tests) {
-        console.info(test.title)
-        try {
-            const ans = successfulPairs(test.input.spells, test.input.potions, test.input.success)
-            expect(ans).toStrictEqual(test.expected)
-        } catch (error) {
-            throw error
-        }
-    }
-})
-
+	for (const test of tests) {
+		console.info(test.title);
+		try {
+			const ans = successfulPairs(
+				test.input.spells,
+				test.input.potions,
+				test.input.success
+			);
+			expect(ans).toStrictEqual(test.expected);
+		} catch (error) {
+			throw error;
+		}
+	}
+});
 
 // yarn jest '2300. Successful Pairs of Spells and Potions/index.test.js'
