ffa5c111689addabcade88893eec9c84b113c0b0
Klay <wkus963@gmail.com>
2023-10-17 16:59:53 +0800

    test foreach

---
+++ LeetCode/C#/All/TestForeach/TestForeach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.TestForeach
+{
+    public class TestForeach
+    {
+        public static void TestForeachFunc() { 
+            var foo = new List<int> { 1, 2 ,3 };
+            var bar = -1000;
+            foo.ForEach(x => {
+                x = 0;
+                bar = x;
+            });
+
+            foo.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+            Console.WriteLine($"bar is {bar}");
+            Console.WriteLine("end");
+        }
+    }
+}
---
+++ LeetCode/C#/AllTests/TestForeach/TestForeachTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All.TestForeach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.TestForeach.Tests
+{
+    [TestClass()]
+    public class TestForeachTests
+    {
+        [TestMethod()]
+        public void TestForeachFuncTest()
+        {
+            //Console.WriteLine("before test");
+            TestForeach.TestForeachFunc();
+            Assert.IsTrue(true);
+        }
+    }
+}
