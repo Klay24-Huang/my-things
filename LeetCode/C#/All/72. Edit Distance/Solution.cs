04d1e80c4a95f3fa13f8f69418a260db512fc788
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-20 11:28:18 +0800

    copy ans

--- LeetCode/C#/All/72. Edit Distance/Solution.cs
+++ LeetCode/C#/All/72. Edit Distance/Solution.cs
@@ -11,44 +11,25 @@ namespace All._72._Edit_Distance
     {
         public int MinDistance(string word1, string word2)
         {
-            if (string.IsNullOrEmpty(word1))
-                return word2.Length;
+            int w1 = word1.Length, w2 = word2.Length;
+            int[] prev = new int[w2 + 1];
+            for (int i = 0; i <= w2; i++)
+                prev[i] = i;
 
-            if (string.IsNullOrEmpty(word2))
-                return word1.Length;
-
-            int[][] dp = new int[word1.Length][];
-
-            for (var i = 0; i < word1.Length; i++)
+            for (int i = 1; i <= w1; i++)
             {
-                dp[i] = new int[word2.Length];
-                var prev = i - 1 < 0 ? 0 : dp[i - 1][0];
-                dp[i][0] = prev + (word1[i] != word2[0] ? 1 : 0);
-            }
-
-            for (var j = 0; j < word2.Length; j++)
-            {
-                var prev = j - 1 < 0 ? 0 : dp[0][j - 1];
-                dp[0][j] = prev + (word1[0] != word2[j] ? 1 : 0);
-            }
-
-            for (var i = 1; i < dp.Length; i++)
-            {
-                for (var j = 1; j < dp[i].Length; j++)
+                int[] curr = new int[w2 + 1];
+                curr[0] = i;
+                for (int j = 1; j <= w2; j++)
                 {
-                    var preMax = new int[] { dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1] }.Min();
-                    if (word1[i] != word2[j])
-                    {
-                        dp[i][j] = 1 + preMax;
-                    }
+                    if (word1[i - 1] == word2[j - 1])
+                        curr[j] = prev[j - 1];
                     else
-                    {
-                        dp[i][j] = preMax;
-                    }
+                        curr[j] = Math.Min(prev[j - 1], Math.Min(prev[j], curr[j - 1])) + 1;
                 }
+                prev = curr;
             }
-            Console.WriteLine(JsonSerializer.Serialize(dp));
-            return dp[word1.Length - 1][word2.Length - 1];
+            return prev[w2];
         }
     }
 }
--- LeetCode/C#/AllTests/72. Edit Distance/SolutionTests.cs
+++ LeetCode/C#/AllTests/72. Edit Distance/SolutionTests.cs
@@ -27,7 +27,7 @@ namespace All._72._Edit_Distance.Tests
                     },
                     Want = 3
                 },
-                 new Test<Args, int>
+                new Test<Args, int>
                 {
                     Name = "case: 2",
                     Args = new Args{
@@ -36,7 +36,7 @@ namespace All._72._Edit_Distance.Tests
                     },
                     Want = 5
                 },
-                  new Test<Args, int>
+                new Test<Args, int>
                 {
                     Name = "case:3",
                     Args = new Args{
@@ -44,6 +44,15 @@ namespace All._72._Edit_Distance.Tests
                         Word2 = "zoogeologist"
                     },
                     Want = 10
+                },
+                new Test<Args, int>
+                {
+                    Name = "case:4",
+                    Args = new Args{
+                        Word1 = "pneumonoultramicroscopicsilicovolcanoconiosis",
+                        Word2 = "ultramicroscopically"
+                    },
+                    Want = 27
                 },
             };
 
@@ -53,7 +62,7 @@ namespace All._72._Edit_Distance.Tests
                 var input = test.Args;
                 var result = s.MinDistance(input.Word1, input.Word2);
                 var errorMessage = $"{test.Name}";
-                
+
                 Assert.AreEqual(test.Want, result, errorMessage);
             }
         }
