cae7ec1832b306fbd0a7248bc90d6019014c7fbb
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-20 14:06:53 +0800

    temporary save

---
+++ LeetCode/C#/All/714. Best Time to Buy and Sell Stock with Transaction Fee/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace All._714._Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices, int fee)
+        {
+            var dp = new int[prices.Length, prices.Length + 1];
+            //for (var i = 0; i < prices.Length; i++)
+            //{
+            //    dp[i, 0] = 0;
+            //}
+
+            //for (var i = 0; i < prices.Length + 1; i++)
+            //{
+            //    dp[0, i] = 0;
+            //}
+
+            for (var i = 1; i < prices.Length; i++)
+            {
+                for (var j = i + 1; j < prices.Length + 1; j++)
+                {
+                    var profit = prices[j - 1] - prices[i - 1] - fee;
+                    //Console.WriteLine($"index i: {i}, j: {j}, raw profit: {profit}");
+                    if (profit < 0)
+                        profit = 0;
+                    if (i - 2 >= 0)
+                    {
+                        //Console.WriteLine($"in ,{dp[i, j]}");
+                        profit += dp[i-2, i-1];
+                    }
+                    //Console.WriteLine($"profit: {profit}");
+                    dp[i, j] = profit;
+                    dp[i, j] = new int[] { dp[i - 1, j], dp[i, j - 1], profit }.Max();
+                    //dp[i, j] = new int[] { dp[i - 1, j], dp[i, j - 1], (profit + dp[i - 1, j - 1]) }.Max();
+                }
+            }
+            //Print2DArray(dp);
+            return dp[prices.Length - 1, prices.Length];
+        }
+
+
+        public static void Print2DArray<T>(T[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ LeetCode/C#/AllTests/714. Best Time to Buy and Sell Stock with Transaction Fee/SolutionTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All._714._Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllTests;
+
+namespace All._714._Best_Time_to_Buy_and_Sell_Stock_with_Transaction_Fee.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MaxProfitTest()
+        {
+            var tests = new List<Test<Args, int>>
+            {
+                new Test<Args, int>
+                {
+                    Name = "case: 1",
+                    Args = new Args{
+                        Prices = new int[] { 1,3,2,8,4,9},
+                        Fee = 2
+                    },
+                    Want = 8
+                },
+                new Test<Args, int>
+                {
+                    Name = "case: 2",
+                    Args = new Args{
+                        Prices = new int[] { 1, 3, 7, 5, 10, 3 },
+                        Fee = 3
+                    },
+                    Want = 6
+                },
+            };
+
+            var s = new Solution();
+            foreach (var test in tests)
+            {
+                var input = test.Args;
+                var result = s.MaxProfit(input.Prices, input.Fee);
+                var errorMessage = $"{test.Name}";
+
+                Assert.AreEqual(test.Want, result, errorMessage);
+            }
+        }
+
+        private class Args
+        {
+            public int[] Prices { get; set; } = Array.Empty<int>();
+            public int Fee { get; set; }
+        }
+    }
+}
