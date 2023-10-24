e1ce2a57d82f55dbf096b7be788dd9c218d41fc2
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-24 16:34:29 +0800

    solove

--- LeetCode/C#/All/2462. Total Cost to Hire K Workers/Solution.cs
+++ LeetCode/C#/All/2462. Total Cost to Hire K Workers/Solution.cs
@@ -3,59 +3,117 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace All._2462._Total_Cost_to_Hire_K_Workers
 {
     public class Solution
     {
+        private class Item
+        {
+            public int Cost { get; set; }
+            public int Index { get; set; }
+        }
+
+        private class PriorityComparere : IComparer<Item> 
+        {
+            public int Compare(Item? x, Item? y)
+            {
+                if (x.Cost != y.Cost) return x.Cost.CompareTo(y.Cost);
+                else return x.Index.CompareTo(y.Index);
+            }
+        }
+
         public long TotalCost(int[] costs, int k, int candidates)
         {
             var len = costs.Length;
-            var pq = new PriorityQueue<int, int>();
+            var pq = new PriorityQueue<Item, Item>(new PriorityComparere());
             var l = 0;
             var r = len - 1;
+
+            //for (var i = 0; i < candidates; i++)
+            //{
+            //    l = i;
+            //    pq.Enqueue(l, costs[l]);
+            //    var newR = len - 1 - i;
+            //    if (newR >  candidates - 1)
+            //    {
+            //        r = newR;
+            //        pq.Enqueue(r, costs[r]);
+            //    }
+            //}
 
             for (var i = 0; i < candidates; i++)
             {
                 l = i;
-                pq.Enqueue(l, costs[l]);
-                var newR = len - 1 - i;
-                if (newR >  candidates - 1)
-                {
-                    r = newR;
-                    pq.Enqueue(r, costs[r]);
-                }
+                var item = new Item {
+                    Index = l,
+                    Cost = costs[l],
+                };
+                pq.Enqueue(item, item);
             }
-            //Console.WriteLine($"left index {l}, right index {r}");
+            Console.WriteLine($"left index {l}, right index {r}, pq count {pq.Count}");
+            for (var i = 0; i < candidates; i++)
+            {
+                var newR = len - 1 - i;
+                if (newR <= candidates - 1)
+                {
+                    break;
+                }
+
+                r = newR;
+                var item = new Item
+                {
+                    Index = r,
+                    Cost = costs[r],
+                };
+                pq.Enqueue(item, item);
+
+            }
+
+
+            //PrintPQ(pq, costs)
+            Console.WriteLine($"left index {l}, right index {r}, pq count {pq.Count}");
             long totalCost = 0;
             for (var i = 0; i < k; i++)
             {
-                var hasItem = pq.TryDequeue(out int index, out int cost);
-
+                //Console.WriteLine($"count of pq {pq.Count}");
+                var hasItem = pq.TryDequeue(out var item, out _);
                 if (hasItem)
                 {
-                    totalCost += cost;
-                    //Console.WriteLine($"add: {cost}, index {index}, total cost {totalCost}");
+                    totalCost += item.Cost;
+                    Console.WriteLine($"add: {item.Cost}, index {item.Index}, total cost {totalCost}");
                     //Console.WriteLine($"{l} {r}");
 
                     // check if there remain item to enqueue
                     if (r - 1 > l)
                     {
-                        if (index - r > 0)
+                        Item newItem;
+                        if (item.Index - r >= 0)
                         {
                             // from right
                             r--;
-                            pq.Enqueue(r, costs[r]);
+                            newItem = new Item
+                            {
+                                Index = r,
+                                Cost = costs[r],
+                            };
                         }
                         else
                         {
                             // from left
                             l++;
-                            pq.Enqueue(l, costs[l]);
+                            newItem = new Item
+                            {
+                                Index = l,
+                                Cost = costs[l],
+                            };
                         }
+                        pq.Enqueue(newItem, newItem);
                     }
                 }
+
                 //Console.WriteLine($"left index {l}, right index {r}");
             }
             return totalCost;
--- LeetCode/C#/AllTests/2462. Total Cost to Hire K Workers/SolutionTests.cs
+++ LeetCode/C#/AllTests/2462. Total Cost to Hire K Workers/SolutionTests.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AllTests;
+using System.Xml.Linq;
 
 namespace All._2462._Total_Cost_to_Hire_K_Workers.Tests
 {
@@ -36,6 +37,36 @@ namespace All._2462._Total_Cost_to_Hire_K_Workers.Tests
                         Candidates = 3,
                     },
                     Want = 4
+                },
+                 new Test<Args, int>
+                {
+                    Name = "case: 3",
+                    Args = new Args{
+                        Costs = new int[] {31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58},
+                        K = 11,
+                        Candidates = 2,
+                    },
+                    Want = 423
+                },
+                 new Test<Args, int>
+                {
+                    Name = "case: 4",
+                    Args = new Args{
+                        Costs = new int[] {2,2,2,2,2,2,1,4,5,5,5,5,5,2,2,2,2,2,2,2,2,2,2,2,2,2},
+                        K = 7,
+                        Candidates = 3,
+                    },
+                    Want = 13
+                },
+                 new Test<Args, int>
+                {
+                    Name = "case: 5",
+                    Args = new Args{
+                        Costs = new int[] {69,10,63,24,1,71,55,46,4,61,78,21,85,52,83,77,42,21,73,2,80,99,98,89,55,94,63,50,43,62,14},
+                        K = 21,
+                        Candidates = 31,
+                    },
+                    Want = 829
                 },
             };
 
