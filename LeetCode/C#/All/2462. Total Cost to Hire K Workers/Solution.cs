b84bff2966fe85d5ed6430208bc9a4c229448939
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-24 11:44:01 +0800

    temporary save, wait to change to single priority queue

--- LeetCode/C#/All/2462. Total Cost to Hire K Workers/Solution.cs
+++ LeetCode/C#/All/2462. Total Cost to Hire K Workers/Solution.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,33 +12,107 @@ namespace All._2462._Total_Cost_to_Hire_K_Workers
         public long TotalCost(int[] costs, int k, int candidates)
         {
             var n = costs.Length;
-            var pgFromStart = new PriorityQueue<int,int>();
-            var pgFromEnd = new PriorityQueue<int,int>();
-            for(var i = 0; i < k; i++)
+            var pqLeft = new PriorityQueue<int, int>();
+            var pqRight = new PriorityQueue<int, int>();
+
+            var l = 0;
+
+            for (; l < candidates;)
             {
-                pgFromStart.Enqueue(i, costs[i]);
-                pgFromEnd.Enqueue(i, costs[n - 1 - i]);
+                pqLeft.Enqueue(l, costs[l]);
+                l++;
+
+            }
+
+            var r = n - 1;
+            for (var i = 0; i < candidates; i++)
+            {
+                if (l > r)
+                {
+                    break;
+                }
+                pqRight.Enqueue(r, costs[r]);
+                r--;
             }
 
             long cost = 0;
             for (var i = 0; i < k; i++)
             {
-                int i1 = -1;
-                int c1 = -1;
-                pgFromStart.TryPeek(out i1, out c1);
+                var rIndex = int.MaxValue;
+                var rCost = int.MaxValue;
+                var rHasItem = pqRight.TryPeek(out rIndex, out rCost);
 
-                int i2 = -1;
-                int c2 = -1;
-                pgFromEnd.TryPeek(out i2, out c2);
-                
-                if (i1< i2)
+                var lIndex = int.MaxValue;
+                var lCost = int.MaxValue;
+                var lHasItem = pqLeft.TryPeek(out lIndex, out lCost);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                if ((lHasItem && !rHasItem) || lCost < rCost || ((lCost == rCost) && (lIndex < rIndex)))
                 {
-                    cost += i1;
-                    pgFromStart.Dequeue();
+                    cost += lCost;
+                    pqLeft.Dequeue();
+                    if (l <= r)
+                    {
+                        pqLeft.Enqueue(l, costs[l]);
+                        Console.WriteLine($"enquue index {l}");
+                        l++;
+                    }
                 }
-            }
 
+                if ((rHasItem && !lHasItem) || rCost < lCost || ((rCost == lCost) && (rIndex < lIndex)))
+                {
+                    cost += rCost;
+                    pqRight.Dequeue();
+                    if (r >= l)
+                    {
+                        pqRight.Enqueue(r, costs[r]);
+                        Console.WriteLine($"enquue index {r}");
+                        r--;
+                    }
+                }
+                Console.WriteLine($"r, index {rIndex}, cost {rCost}");
+
+                Console.WriteLine($"l, index {lIndex}, cost {lCost}");
+                Console.WriteLine($"after round. cost is {cost}");
+            }
             return cost;
+            //var n = costs.Length;
+            //var pqFromStart = new PriorityQueue<int,int>();
+            //var pqFromEnd = new PriorityQueue<int,int>();
+            //for(var i = 0; i < k; i++)
+            //{
+            //    pqFromStart.Enqueue(i, costs[i]);
+            //    pqFromEnd.Enqueue(i, costs[n - 1 - i]);
+            //    pqFromStart.con
+            //}
+
+            //long cost = 0;
+            //for (var i = 0; i < k; i++)
+            //{
+            //    int i1 = -1;
+            //    int c1 = -1;
+            //    pqFromStart.TryPeek(out i1, out c1);
+
+            //    int i2 = -1;
+            //    int c2 = -1;
+            //    pqFromEnd.TryPeek(out i2, out c2);
+
+            //    if (c1< c2)
+            //    {
+            //        cost += c1;
+            //        pqFromStart.Dequeue();
+            //        pqFromStart.Enqueue(i1 + 1, costs[i1 + 1]);
+            //    }
+            //    else
+            //    {
+            //        cost += c2;
+            //        pqFromEnd.Dequeue();
+            //        pqFromEnd.Enqueue(i2 + 1, costs[i2 + 1]);
+            //    }
+            //}
+
+            //return cost;
         }
     }
 }
---
+++ LeetCode/C#/AllTests/2462. Total Cost to Hire K Workers/SolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All._2462._Total_Cost_to_Hire_K_Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllTests;
+
+namespace All._2462._Total_Cost_to_Hire_K_Workers.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void TotalCostTest()
+        {
+            var tests = new List<Test<Args, int>>
+            {
+                //new Test<Args, int>
+                //{
+                //    Name = "case: 1",
+                //    Args = new Args{
+                //        Costs = new int[] {17,12,10,2,7,2,11,20,8},
+                //        K = 3,
+                //        Candidates = 4,
+                //    },
+                //    Want = 11
+                //},
+                 new Test<Args, int>
+                {
+                    Name = "case: 2",
+                    Args = new Args{
+                        Costs = new int[] {1,2,4,1},
+                        K = 3,
+                        Candidates = 3,
+                    },
+                    Want = 4
+                },
+            };
+
+            var s = new Solution();
+            foreach (var test in tests)
+            {
+                Console.WriteLine(test.Name);
+                var input = test.Args;
+                var result = s.TotalCost(input.Costs, input.K, input.Candidates);
+                var errorMessage = $"{test.Name}";
+
+                Assert.AreEqual(test.Want, result, errorMessage);
+            }
+        }
+
+        private class Args
+        {
+            public int[] Costs { get; set; } = Array.Empty<int>();
+            public int K { get; set; }
+            public int Candidates { get; set; }
+        }
+    }
+}
