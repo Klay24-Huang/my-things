fdea893471e76ec3aba117130a73e256485a5bfb
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-25 11:56:22 +0800

    write wrong answer

---
+++ LeetCode/C#/All/1493. Longest Subarray of 1's After Deleting One Element/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace All._1493._Longest_Subarray_of_1_s_After_Deleting_One_Element
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            var ans = 0;
+            var holder = new List<int>();
+            var add  = (int num) => { 
+                holder.Add(num);
+                if (holder.Count > 3)
+                {
+                    holder.RemoveAt(0);
+                }
+            };
+
+            var getAns = () =>
+            {
+                Console.WriteLine(JsonSerializer.Serialize(holder));
+                var r = holder.Sum();
+                ans = r > ans ? r : ans;
+            };
+
+            var currLen = 0;
+            foreach (var item in nums)
+            {
+                if (item == 0)
+                {
+                    if(currLen > 0)
+                    {
+                        add(currLen);
+                        getAns();
+                        currLen = 0;
+                    }
+                    add(0);
+                    getAns();
+                }
+                else
+                {
+                    // is one
+                    currLen++;
+                }
+            }
+
+            if(currLen > 0)
+            {
+                add(currLen);
+                getAns();
+            }
+            
+            return ans;
+        }
+    }
+}
---
+++ LeetCode/C#/AllTests/1493. Longest Subarray of 1's After Deleting One Element/SolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All._1493._Longest_Subarray_of_1_s_After_Deleting_One_Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllTests;
+
+namespace All._1493._Longest_Subarray_of_1_s_After_Deleting_One_Element.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void LongestSubarrayTest()
+        {
+            var tests = new List<Test<Args, int>>
+            {
+                new Test<Args, int>
+                {
+                    Name = "case: 1",
+                    Args = new Args{
+                        Nums = new int[] {1,1,0,1 },
+                    },
+                    Want = 3,
+                },
+                new Test<Args, int>
+                {
+                    Name = "case: 2",
+                    Args = new Args{
+                        Nums = new int[] {0,1,1,1,0,1,1,0,1 },
+                    },
+                    Want = 5,
+                },
+                new Test<Args, int>
+                {
+                    Name = "case: 3",
+                    Args = new Args{
+                        Nums = new int[] {1,1,1 },
+                    },
+                    Want = 2,
+                },
+            };
+
+            var s = new Solution();
+            foreach (var test in tests)
+            {
+                Console.WriteLine($"case: {test.Name}");
+                var input = test.Args;
+                var result = s.LongestSubarray(input.Nums);
+                Assert.AreEqual(test.Want, result);
+            }
+        }
+    }
+
+    public class Args
+    {
+        public int[] Nums { get; set; } = Array.Empty<int>();
+    }
+}
