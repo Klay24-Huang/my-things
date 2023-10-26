34044cb213281f0bdad56734614af8ce71707d19
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-26 15:03:29 +0800

    solve

---
+++ LeetCode/C#/All/1657. Determine if Two Strings Are Close/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All._1657._Determine_if_Two_Strings_Are_Close
+{
+    public class Solution
+    {
+        public bool CloseStrings(string word1, string word2)
+        {
+            var d = new Dictionary<char, int>();
+            foreach (var word in word1)
+            {
+                if (d.ContainsKey(word))
+                {
+                    d[word]++;
+                }
+                else
+                { d[word]++; }
+            }
+        }
+    }
+}
