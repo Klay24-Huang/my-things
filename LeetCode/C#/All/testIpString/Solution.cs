54661a500b13ac2258e689e56ddc48f5c2efd779
NTDOMAIN\HICEIT6 <HICEIT6@hotaimotor.com.tw>
2023-10-27 15:13:45 +0800

    .

---
+++ LeetCode/C#/All/testIpString/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.testIpString
+{
+    public class Solution
+    {
+        public void Test()
+        {
+            var ips = new List<string> {
+                "211.72.238.168, 147.243.138.115:50526",
+                "2401:e180:8883:9c50:6c30:4189:58c4:3066, 147.243.31.203:44609"
+            };
+
+            foreach (var ip in ips)
+            {
+                Console.WriteLine(ip.Split(new char[] { ',' }).FirstOrDefault());
+            }
+        }
+    }
+}
---
+++ LeetCode/C#/AllTests/testIpString/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All.testIpString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All.testIpString.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void TestTest()
+        {
+            Solution solution = new Solution();
+            solution.Test();
+            Assert.Fail();
+        }
+    }
+}
