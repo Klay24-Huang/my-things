6f97a728a2befedcf5c77ac0f6d1cf3a5f9189b7
Hao Husiang <HICEIT6@hotaimotor.com.tw>
2024-01-04 16:30:29 +0800

    test with sp test db

---
+++ C#/SSDT-Test/Database1/dbo/Tables/Assignment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Assignment] (
+    [AssignmentId] INT IDENTITY (1, 1) NOT NULL,
+    [EmployeeId]   INT NOT NULL,
+    [TaskId]       INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([AssignmentId] ASC),
+    FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([EmployeeId]),
+    FOREIGN KEY ([TaskId]) REFERENCES [dbo].[Task] ([TaskId])
+);
+
---
+++ C#/SSDT-Test/Database1/dbo/Tables/Task.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Task] (
+    [TaskId]     INT           IDENTITY (1, 1) NOT NULL,
+    [ProjectId]  INT           NOT NULL,
+    [TaskName]   VARCHAR (255) NULL,
+    [Priority]   VARCHAR (50)  NULL,
+    [TaskStatus] VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([TaskId] ASC),
+    FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([ProjectId])
+);
+
