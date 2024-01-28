a6ab97c077e000f1e872fb0f9972305171a346a7
HOME\sean2 <wkus963@gmail.com>
2024-01-28 13:22:58 +0800

    async example

---
+++ C#/dotnet-vscode-test/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            Task.Run(() => FooAsync()); // Run FooAsync on a separate thread
+            return "OK";
+        }
+
+        private void FooAsync()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                System.Diagnostics.Debug.WriteLine($"Loop count: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
