4287cd21a7cdc0d76fca22c31f17f8f3e1481090
Hao Husiang <HICEIT6@hotaimotor.com.tw>
2024-02-15 16:45:37 +0800

    .

---
+++ C#/HashsetTest/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> myHashSet = new HashSet<string>();
+
+        // 測試加入不存在的字串
+        AddIfNotExists(myHashSet, "apple");
+        AddIfNotExists(myHashSet, "banana");
+        AddIfNotExists(myHashSet, "apple");  // 已經存在，不會再次加入
+
+        // 輸出集合中的字串
+        Console.WriteLine("HashSet Contents:");
+        foreach (var item in myHashSet)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static void AddIfNotExists(HashSet<string> hashSet, string value)
+    {
+        if (!hashSet.Contains(value))
+        {
+            hashSet.Add(value);
+            Console.WriteLine($"Added: {value}");
+        }
+        else
+        {
+            Console.WriteLine($"Already exists: {value}");
+        }
+    }
+}
